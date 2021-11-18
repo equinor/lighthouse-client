4d241342085b7003c12ebaaf72d7e4fed6d030ed
Gustav-Eikaas <guei@equinor.com>
2021-11-18 08:36:32 +0100

    persists expanded group

--- src/components/Garden/Components/Group.tsx
+++ src/components/Garden/Components/Group.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { Count, Pack } from '../Styles/gardenStyle';
 import { ChevronUp, ChevronDown } from '../Icons/Chevron';
@@ -11,22 +11,23 @@ interface GroupProps<T> {
 }
 
 export function Group<T>({ group, itemKey }: GroupProps<T>): JSX.Element {
-    const [isExpanded, setIsExpanded] = useState<boolean>(false);
-    const handleClick = (data: DataSet<T>): void => {
-        setIsExpanded((prev) => !prev);
+    const [isExpanded, setIsExpanded] = useState(group.isExpanded);
+    const handleClick = () => {
+        setIsExpanded((prev) => (!prev));
+        group.isExpanded = !group.isExpanded;
     }
 
-    useEffect(() => {
-    }, [group, isExpanded])
+
+
 
     return (
         <>
-            <Pack key={group.value} onClick={() => handleClick(group)}>
+            <Pack key={group.value} onClick={() => handleClick()}>
                 {group.value}
                 <Count>({group.count})</Count>
-                {isExpanded ? <ChevronUp /> : <ChevronDown />}
+                {group.isExpanded ? <ChevronUp /> : <ChevronDown />}
             </Pack>
-            {isExpanded && (group.items[0] != null ? <Items data={group.items} itemKey={itemKey} /> :
+            {group.isExpanded && (group.items[0] != null ? <Items data={group.items} itemKey={itemKey} /> :
                 <> {Object.values(group.subGroups).map((x) => (<Group group={x} itemKey={itemKey} />))}</>)}
         </>
     )
