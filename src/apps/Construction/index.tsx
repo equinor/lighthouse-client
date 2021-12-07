2c1beea2cc05a98fcc873ca2963fd121cdab2e78
Christopher Berge Hove <chbh@equinor.com>
2021-12-07 21:14:06 +0100

    iframe embed prower bi test

--- src/apps/Construction/index.tsx
+++ src/apps/Construction/index.tsx
@@ -9,14 +9,23 @@ export function setup(api: AppApi): void {
 
     construction.registerFusionPowerBi('test', {
         title: 'test',
-        reportURI: 'lci-hanging-gardens',
+        reportURI: '7a8be422-7446-488a-9aa6-1d1e52bf93ce',
     });
     construction.registerFusionPowerBi('test2', {
         title: 'Test2',
-        reportURI: 'lci-hanging-gardens',
+        reportURI: '7a8be422-7446-488a-9aa6-1d1e52bf93ce',
     });
     construction.registerCustom('test3', {
         title: 'Test3',
-        component: () => <div>Hello World!</div>,
+        component: () => (
+            <iframe
+                title="Scaffolding analytics - Overview"
+                width="100%"
+                height="800"
+                src="https://app.powerbi.com/reportEmbed?reportId=7a8be422-7446-488a-9aa6-1d1e52bf93ce&autoAuth=true&ctid=3aa4a235-b6e2-48d5-9195-7fcf05b459b0&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLW5vcnRoLWV1cm9wZS1oLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
+                frameBorder="0"
+                allowFullScreen={true}
+            ></iframe>
+        ),
     });
 }
