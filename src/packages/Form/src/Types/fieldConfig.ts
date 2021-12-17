5befe75705554802d5cf0deedff30614919c99ea
Gustav-Eikaas <guei@equinor.com>
2021-12-17 12:49:39 +0100

    description for fieldConfig

--- src/packages/Form/src/Types/fieldConfig.ts
+++ src/packages/Form/src/Types/fieldConfig.ts
@@ -1,10 +1,16 @@
 import { InputType } from './InputTypes/inputType';
 
 export interface FieldConfig<T> {
+    /** Title to be shown above the input field */
     title: string;
+    /** Input type */
     inputType: InputType;
+    /** Order of the input fields, give two the same order if you want them side by side */
     order?: number;
+    /** Is field optional, default false */
     optional?: boolean;
+    /** Should field be editable when form is in editmode, default true */
     editable?: boolean;
+    /** Takes in the value of the field and lets you write your own validation function */
     validationFunction?: (value: T | undefined) => boolean;
 }
