1309daa70c6810af88bd19449a18376d638a8331
Gustav-Eikaas <guei@equinor.com>
2021-12-22 13:31:54 +0100

    Disable workorder tab

--- src/apps/Construction/index.tsx
+++ src/apps/Construction/index.tsx
@@ -94,59 +94,59 @@ export function setup(appApi: AppApi): void {
     //     reportURI: 'swcr-analytics-rls',
     // });
 
-    const workPreparation = construction.registerDashboard<WP>('work-preparation', {
-        title: 'Work Preparation',
-    });
+    // const workPreparation = construction.registerDashboard<WP>('work-preparation', {
+    //     title: 'Work Preparation',
+    // });
 
-    // Loop Data Test for testing system..
-    workPreparation.registerDataSource(async () => {
-        const plantId = 'PCS$JOHAN_CASTBERG';
-        const project = 'L.O532C.002';
-        const response = await api.fetch(
-            `https://api-lighthouse-production.playground.radix.equinor.com/loops/${plantId}/${project}`
-        );
+    // // Loop Data Test for testing system..
+    // workPreparation.registerDataSource(async () => {
+    //     const plantId = 'PCS$JOHAN_CASTBERG';
+    //     const project = 'L.O532C.002';
+    //     const response = await api.fetch(
+    //         `https://api-lighthouse-production.playground.radix.equinor.com/loops/${plantId}/${project}`
+    //     );
 
-        return JSON.parse(await response.text());
-    });
+    //     return JSON.parse(await response.text());
+    // });
 
-    const excludeKeys: (keyof WP)[] = [
-        'tagNo',
-        'functionTags',
-        'contentChecklists',
-        'description',
-        'commPk',
-        'mcPk',
-        'signedAt',
-        'createdAt',
-    ];
+    // const excludeKeys: (keyof WP)[] = [
+    //     'tagNo',
+    //     'functionTags',
+    //     'contentChecklists',
+    //     'description',
+    //     'commPk',
+    //     'mcPk',
+    //     'signedAt',
+    //     'createdAt',
+    // ];
 
-    workPreparation.registerFilterOptions({ excludeKeys });
+    // workPreparation.registerFilterOptions({ excludeKeys });
 
-    workPreparation.registerPage({
-        title: 'Jobcards',
-        pageId: 'workPreparationJobCards',
-        type: 'AnalyticsPage',
-        ...analyticsOptions,
-    });
+    // workPreparation.registerPage({
+    //     title: 'Jobcards',
+    //     pageId: 'workPreparationJobCards',
+    //     type: 'AnalyticsPage',
+    //     ...analyticsOptions,
+    // });
 
-    workPreparation.registerPage({
-        title: 'Hours',
-        pageId: 'workPreparationHours',
-        type: 'AnalyticsPage',
-        ...analyticsOptions2,
-    });
-    workPreparation.registerPage({
-        title: 'Details',
-        pageId: 'workPreparationDetails',
-        type: 'AnalyticsPage',
-        ...analyticsOptions,
-    });
-    workPreparation.registerPage({
-        title: 'Hold',
-        pageId: 'workPreparationDetailsHold',
-        type: 'AnalyticsPage',
-        ...analyticsOptions2,
-    });
+    // workPreparation.registerPage({
+    //     title: 'Hours',
+    //     pageId: 'workPreparationHours',
+    //     type: 'AnalyticsPage',
+    //     ...analyticsOptions2,
+    // });
+    // workPreparation.registerPage({
+    //     title: 'Details',
+    //     pageId: 'workPreparationDetails',
+    //     type: 'AnalyticsPage',
+    //     ...analyticsOptions,
+    // });
+    // workPreparation.registerPage({
+    //     title: 'Hold',
+    //     pageId: 'workPreparationDetailsHold',
+    //     type: 'AnalyticsPage',
+    //     ...analyticsOptions2,
+    // });
 
     /** 
     Remove LCI hanging garden, since its not relevant for Construction
