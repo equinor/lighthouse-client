bcf0dbf0befaf5260087441eb397555cd5f866a6
Christopher Berge Hove <chbh@equinor.com>
2022-01-13 13:45:14 +0100

    env

--- setting.ts
+++ setting.ts
@@ -1,5 +1,5 @@
 export const env = {
     funcUrl: 'https://func-ppo-web-client-dev.azurewebsites.net/api/LighthouseConfig',
     // baseUrlProxy: 'https://lih-proxy.azurewebsites.net',
-    baseUrlProxy: 'https://app-ppo-proxy-dev.azurewebsites.net',
+    FunctionEnvironment: 'https://app-ppo-proxy-dev.azurewebsites.net',
 };
