c00e051821a2e0cf1f4e8f45126a894cf3f8a3db
Christopher Berge Hove <chbh@equinor.com>
2022-02-02 14:05:22 +0100

    Env on title and other chores (#138)
    * Updates showing envi in title and top badge.
    
    * open in link in new tab and lin on mega menu

--- src/Core/Client/Service/appConfig.ts
+++ src/Core/Client/Service/appConfig.ts
@@ -6,11 +6,11 @@ import { fetchClientConfig } from './envConfig';
 export async function fetchConfig(): Promise<AppConfigResult> {
     const config = await fetchClientConfig();
     const isProduction = config.CLIENT_ENV === 'prod';
-
+    document.title = `${config.CLIENT_ENV.toLocaleUpperCase()} | Fusion | Johan Castberg | Pilot`;
     setEnv(isProduction, config.CLIENT_ENV);
 
     const response = await fetch(getEnvironmentUri(config.ENV_CONFIG_URI, config.CLIENT_ENV));
-    return { ...(await response.json()), isProduction };
+    return { ...(await response.json()), isProduction, env: config.CLIENT_ENV };
 }
 
 function getEnvironmentUri(baseUri: string, env: string): string {
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,50 +1,15 @@
 import { Avatar, TopBar } from '@equinor/eds-core-react';
 import { tokens } from '@equinor/eds-tokens';
 import { useClientContext } from '@equinor/portal-client';
-import styled from 'styled-components';
 import { SupportButton } from '../../Core/Client/Support/Support';
 import Icon from '../Icon/Icon';
 import Logo from './Logo/Logo';
-
-const Icons = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: row-reverse;
-    > * {
-        margin-left: 40px;
-    }
-`;
-
-const TopBarWrapper = styled(TopBar)`
-    position: fixed;
-    width: 100%;
-    z-index: 2;
-    height: 48px;
-    padding-left: 12px;
-    padding-right: 12px;
-    > header {
-        padding-left: 1.5rem;
-    }
-`;
-
-const BetaTag = styled.div`
-    padding: 0 1rem;
-    position: absolute;
-    left: 40%;
-    top: 0px;
-    left: 500px;
-    display: flex;
-    background: #ff7e29;
-    color: #fff;
-    align-items: center;
-    justify-content: space-between;
-    width: 500px;
-`;
+import { BetaTag, Icons, TopBarWrapper } from './TopBarStyle';
 
 const ClientTopBar = (): JSX.Element => {
     const {
         toggleFullscreenMenu,
-        settings: { userImageUrl },
+        settings: { userImageUrl, clientEnv },
     } = useClientContext();
 
     return (
@@ -61,12 +26,8 @@ const ClientTopBar = (): JSX.Element => {
                 <Logo />
             </TopBar.Header>
             <TopBar.CustomContent>
-                {/* <CustomContentWrapper>
-                        <PlantSelector />
-                        <Search aria-label="sitewide" id="search-normal" placeholder="Search..." />
-                    </CustomContentWrapper> */}
                 <BetaTag>
-                    <b>UNDER DEVELOPMENT - </b>
+                    <b>UNDER DEVELOPMENT - {clientEnv.toUpperCase()} </b>
                     <p>This site contains test data.</p>
                 </BetaTag>
             </TopBar.CustomContent>
@@ -77,8 +38,6 @@ const ClientTopBar = (): JSX.Element => {
                     ) : (
                         <Avatar alt="User avatar" src={userImageUrl} />
                     )}
-
-                    {/* <Icon name="support" color={tokens.colors.interactive.disabled__text.rgba} /> */}
                 </Icons>
             </TopBar.Actions>
             <SupportButton />
