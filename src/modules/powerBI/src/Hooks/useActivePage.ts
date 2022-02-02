4b3dbb7e6ccb802b0823757559e5010d452bddd4
Joachim Senland <joachim290@gmail.com>
2022-02-02 10:45:10 +0100

    [PowerBI] - Feature/powerbi custom page navigation (#123)

---
+++ src/modules/powerBI/src/Hooks/useActivePage.ts
@@ -0,0 +1,36 @@
+import { Page, Report } from 'powerbi-client';
+import { useCallback, useEffect, useState } from 'react';
+
+/**
+ * Will get the active page at first render.
+ * If user changes page, it should be handled by `setActivePage`.
+ */
+export const useActivePage = (
+    report?: Report
+): [activePage: Page | undefined, setActivePage: (page: Page) => void] => {
+    const [activePage, setActivePage] = useState<Page>();
+
+    const handleChange = useCallback(
+        (page: Page) => {
+            report?.setPage(page.name);
+            setActivePage(page);
+        },
+        [report]
+    );
+
+    useEffect(() => {
+        report &&
+            !activePage &&
+            report.on('rendered', async () => {
+                try {
+                    const active = await report.getActivePage();
+
+                    setActivePage(active);
+                } catch {
+                    console.error('Cannot retrieve active page');
+                }
+            });
+    }, [activePage, report]);
+
+    return [activePage, handleChange];
+};
