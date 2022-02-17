2411ea6e22766daf81b9e0dccf5241bb9f19748c
Gustav-Eikaas <guei@equinor.com>
2022-02-17 10:53:48 +0100

    cr fix

--- src/apps/ScopeChangeRequest/Hooks/usePreloadCaching.ts
+++ src/apps/ScopeChangeRequest/Hooks/usePreloadCaching.ts
@@ -4,11 +4,12 @@ import { QueryKeys } from '../Api/ScopeChange/queryKeys';
 import { getSystems } from '../Api/PCS/getSystems';
 import { getFunctionalRoles } from '../Api/PCS/getFunctionalRoles';
 
+//10 hour cache
+const LONG_CACHE = 10 * 1000 * 60 * 60;
+
 /**
  * Preloads api calls that should never invalidate
  */
-//10 hour cache
-const LONG_CACHE = 10 * 1000 * 60 * 60;
 export function usePreloadCaching(): void {
     useQuery(QueryKeys.Systems, getSystems, {
         staleTime: LONG_CACHE,
