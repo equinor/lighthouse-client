60086b4f249def6a663336a81304ddc52cfdfbf6
Gustav-Eikaas <guei@equinor.com>
2022-02-18 14:11:19 +0100

    fixed KPI styling

--- src/packages/StatusBar/src/Components/StatusItem/StatusItem.tsx
+++ src/packages/StatusBar/src/Components/StatusItem/StatusItem.tsx
@@ -1,38 +1,56 @@
 import { tokens } from '@equinor/eds-tokens';
-import { useMemo } from 'react';
-import { StatusCard, Title, Value } from './StatusItemStyles';
+import styled from 'styled-components';
 
 export interface StatusItem {
     title: string;
     value: () => string;
+    weeklyChange?: () => string;
     description?: string;
-    status: keyof Status;
 }
 
-export interface Status {
-    waring: string;
-    info: string;
-    ok: string;
-    default: string;
-}
-
-export function Item({ status, title, value }: React.PropsWithChildren<StatusItem>): JSX.Element {
-    const colors: Status = useMemo(
-        () => ({
-            waring: `${tokens.colors.interactive.danger__resting.hex}`,
-            info: `${tokens.colors.interactive.warning__resting.hex}`,
-            ok: `${tokens.colors.interactive.success__resting.hex}`,
-            default: `${tokens.colors.interactive.primary__resting.hex}`,
-        }),
-        []
-    );
-
+export function Item({
+    title,
+    value,
+    weeklyChange,
+}: React.PropsWithChildren<StatusItem>): JSX.Element {
     return (
-        <StatusCard color={colors[status]}>
-            <div>
-                <Title>{title}</Title>
-            </div>
-            <Value>{value()}</Value>
+        <StatusCard>
+            <Title>{title}</Title>
+            <ValueWrapper>
+                <Value>{value()}</Value>
+                {weeklyChange && <WeeklyChange>({weeklyChange()})</WeeklyChange>}
+            </ValueWrapper>
         </StatusCard>
     );
 }
+
+const Value = styled.div`
+    color: ${tokens.colors.text.static_icons__default.hex};
+    font-size: 20px;
+    line-height: 24px;
+`;
+
+const ValueWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 0.2em;
+`;
+
+const WeeklyChange = styled.div`
+    font-size: 14px;
+    color: ${tokens.colors.text.static_icons__tertiary.hex};
+    line-height: 14px;
+`;
+
+const Title = styled.div`
+    color: ${tokens.colors.text.static_icons__tertiary.hex};
+    font-size: 12px;
+    line-height: 16px;
+`;
+
+const StatusCard = styled.div`
+    min-height: 40;
+    min-width: 70;
+    width: fit-content;
+`;
