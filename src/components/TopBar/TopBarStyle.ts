dc41353c6b04d605243a671df5f545fdd9302cb7
Christopher Berge Hove <chbh@equinor.com>
2022-02-18 14:52:50 +0100

    chore/tasks and devbar (#202)
    * tasks update and dev bar init
    
    * hot fix for swcr in dev
    
    * removed task api file not used

--- src/components/TopBar/TopBarStyle.ts
+++ src/components/TopBar/TopBarStyle.ts
@@ -21,18 +21,3 @@ export const TopBarWrapper = styled(TopBar)`
         padding-left: 1.5rem;
     }
 `;
-
-export const BetaTag = styled.div`
-    padding: 0 1rem;
-    position: absolute;
-    left: 40%;
-    top: 0px;
-    left: 33%;
-    display: flex;
-    background: #ff7e29;
-    color: #fff;
-    align-items: center;
-    justify-content: space-between;
-    width: 30%;
-    min-width: 400px;
-`;
