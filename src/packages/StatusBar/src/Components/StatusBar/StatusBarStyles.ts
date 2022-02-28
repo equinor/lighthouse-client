784a24ad134e56bc4cf9b2def917620bd40281c4
Gustav-Eikaas <guei@equinor.com>
2022-02-28 15:18:47 +0100

    increased spacing between KPI

--- src/packages/StatusBar/src/Components/StatusBar/StatusBarStyles.ts
+++ src/packages/StatusBar/src/Components/StatusBar/StatusBarStyles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StatusWrapper = styled.section`
     display: flex;
-    gap: 1em;
+    gap: 2em;
     min-height: 40px;
     align-items: center;
     margin: 0.3em 1.3em;
