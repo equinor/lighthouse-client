f09d8075e09c9afb7c37cbf9c3dc38b2e5bb2577
Joachim <joachim290@outlook.com>
2022-03-04 09:33:27 +0100

    fix: add isproduction check for contextid

--- src/apps/Handover/Garden/hooks/useHandoverResource.ts
+++ src/apps/Handover/Garden/hooks/useHandoverResource.ts
@@ -1,3 +1,4 @@
+import { httpClient, isProduction } from '@equinor/portal-client';
 import { useCallback, useEffect, useState, useMemo } from 'react';
 import { useHttpClient } from '../../../../Core/Client/Hooks';
 import { HandoverResourceTypeMap } from '../models/handoverResources';
@@ -13,7 +14,9 @@ const useHandoverResource = <T extends keyof HandoverResourceTypeMap>(
 ): UseHandoverResource<T> => {
     const [data, setData] = useState<HandoverResourceTypeMap[T][]>([]);
     const [dataIsFetching, setDataIsFetching] = useState<boolean>(false);
-
+    const contextId = isProduction()
+        ? '65728fee-185d-4a0c-a91d-8e3f3781dad8'
+        : '71db33bb-cb1b-42cf-b5bf-969c77e40931';
     /** 
     This causes an infinite render loop when added as dependency to getData.
      apiClient should be stable, but does not look to be the case.   
@@ -25,7 +28,7 @@ const useHandoverResource = <T extends keyof HandoverResourceTypeMap>(
         setDataIsFetching(true);
         try {
             const result = await fusionApi.fetch(
-                `https://pro-s-dataproxy-fprd.azurewebsites.net/api/contexts/3380fe7d-e5b7-441f-8ce9-a8c3133ee499/handover/${packageId}/${packageType}`
+                `api/contexts/${contextId}/handover/${packageId}/${packageType}`
             );
 
             const parsedSignatures = JSON.parse(
