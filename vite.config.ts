ea1f3623a38e0641f9a743fd6db875a9e8de4d86
Christopher Berge Hove <chbh@equinor.com>
2022-03-08 08:33:22 +0100

    removed old 3d

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,6 @@ export default defineConfig({
             '@equinor/ErrorBoundary': path.resolve(__dirname, './src/Core/ErrorBoundary'),
             '@equinor/StatusBar': path.resolve(__dirname, './src/packages/StatusBar'),
             '@equinor/Diagrams': path.resolve(__dirname, './src/packages/Diagrams'),
-            '@equinor/ThreeDViewer': path.resolve(__dirname, './src/packages/ThreeDViewer'),
             '@equinor/Table': path.resolve(__dirname, './src/packages/Table'),
             '@equinor/authentication': path.resolve(__dirname, './src/packages/authentication'),
             '@equinor/http-client': path.resolve(__dirname, './src/Core/httpClient/'),
