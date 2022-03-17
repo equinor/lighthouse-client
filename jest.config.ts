ce5c31591a7a91e1c05851386a961a24b7b37b66
Joachim <joachim290@outlook.com>
2022-03-17 11:08:08 +0100

    feat: add support for jest testing

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+/**@type {import ('ts-jest').InitialOptionsTsJest} */
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    modulePathIgnorePatterns: ['<rootDir>/packages'],
+};
