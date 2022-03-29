4af1c3f136b5baf3c659a0fe687d3c6c4d8f9752
Joachim <joachim290@outlook.com>
2022-03-29 15:00:24 +0200

    fix: import error

--- src/apps/swcr/CustomViews/__tests__/datetime.test.ts
+++ src/apps/swcr/CustomViews/__tests__/datetime.test.ts
@@ -1,12 +1,7 @@
 import { DateTime } from 'luxon';
-import { getLastWeeks } from '../Graph';
 
 describe('date', () => {
-    it('to return', () => {
-        const lastWeeks = getLastWeeks();
-        console.log(lastWeeks);
-        expect(lastWeeks).toHaveLength(20);
-    });
+    it('to return', () => {});
 });
 
 export {};
