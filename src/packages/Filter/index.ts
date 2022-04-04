03ebdad428a0ab8c82880f5ced3eea3374037fbb
Gustav-Eikaas <guei@equinor.com>
2022-04-04 08:08:00 +0200

    build fix

--- src/packages/Filter/index.ts
+++ src/packages/Filter/index.ts
@@ -1,7 +1,6 @@
 // View Components
 export * from './Components/FilterGroup/FilterGroup';
 export * from './Components/FilterView/FilterView';
-export * from './Components/PopoutFilter/PopupFilter';
 // Context Provider
 export * from './Hooks/useFilterApi';
 // Hooks
