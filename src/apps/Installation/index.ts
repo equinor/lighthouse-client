a03654f9687e6442ef1f4d2ed23af584979135b3
Christopher Berge Hove <chbh@equinor.com>
2022-04-05 14:37:03 +0200

    removed Progress Status sins its not in production

--- src/apps/Installation/index.ts
+++ src/apps/Installation/index.ts
@@ -7,7 +7,7 @@ export function setup(appApi: ClientApi): void {
             reportURI: 'pp-installation-activities',
             pages: [
                 { pageTitle: 'Activities', pageId: 'ReportSection', default: true },
-                { pageTitle: 'Progress Status', pageId: 'ReportSection1bfb8548a2e154dc3640' },
+                // { pageTitle: 'Progress Status', pageId: 'ReportSection1bfb8548a2e154dc3640' },
                 // { pageTitle: 'Plan', pageId: 'ReportSection5b84ed4e7bba01fc9a74' }, May come in the future!
             ],
         })
