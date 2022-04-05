0db20355c756fb84f9dc530848000c0e64e69297
Christopher Berge Hove <chbh@equinor.com>
2022-04-05 12:57:48 +0200

    [Workspace] - Compact filter styles - Atle Style (#352)
    * compact filter styles Atle Style
    
    * styles

--- src/packages/Filter/Components/FilterGroup/FilterGroupStyles.ts
+++ src/packages/Filter/Components/FilterGroup/FilterGroupStyles.ts
@@ -1,5 +1,4 @@
-import { Button } from '@equinor/eds-core-react';
-import { tokens } from '@equinor/eds-tokens';
+import { Button, Checkbox } from '@equinor/eds-core-react';
 import styled from 'styled-components';
 
 export const Title = styled.h4`
@@ -12,17 +11,15 @@ export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     padding: 0.5em;
-    width: fit-content;
+    padding-right: 0;
+    width: auto;
     max-width: 500px;
     text-overflow: ellipsis;
-    min-width: 200px;
     height: fit-content;
     word-wrap: break-word;
     max-height: 180px;
-    /* background-color: ${tokens.colors.ui.background__light.rgba}; */
 
     label {
-        font-size: 1rem;
         padding: 0;
         white-space: nowrap;
         overflow: hidden;
@@ -38,9 +35,13 @@ export const Wrapper = styled.div`
 `;
 
 export const FilterGroupWrapper = styled.div`
-    margin-top: 0.5rem;
     overflow-x: hidden;
     overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        height: 0.1rem;
+        width: 0.3rem;
+    }
 `;
 export const FilterItemWrapper = styled.div`
     padding-bottom: 1rem;
@@ -55,4 +56,21 @@ export const FilterHeaderGroup = styled.div`
 export const SearchButton = styled(Button)`
     width: 36px;
     height: 36px;
+`;
+
+export const AllCheckbox = styled(Checkbox)`
+    grid-template-columns: auto 1fr auto;
+    display: grid;
+    max-width: 500px;
+    align-items: center;
+    padding-top: 2px;
+    padding-bottom: 2px;
+    span {
+        font-size: 13px;
+        padding: 0px;
+
+        :first-child {
+            padding-right: 2px;
+        }
+    }
 `;
