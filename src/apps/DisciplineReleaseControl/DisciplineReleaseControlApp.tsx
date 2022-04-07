f2cdbe95167ad824abc83134f05683765297dd59
Gustav-Eikaas <guei@equinor.com>
2022-04-07 13:02:11 +0200

    filter sort release control

--- src/apps/DisciplineReleaseControl/DisciplineReleaseControlApp.tsx
+++ src/apps/DisciplineReleaseControl/DisciplineReleaseControlApp.tsx
@@ -21,7 +21,7 @@ import {
     getStatusLetterFromStatus,
 } from './Functions/tableHelpers';
 import { getGardenItemColor, getTimePeriod } from './Components/Garden/gardenFunctions';
-import { PipetestStep } from './Types/drcEnums';
+import { PipetestStatusOrder, PipetestStep } from './Types/drcEnums';
 import { DateTime } from 'luxon';
 import { statusBarConfig } from './Components/StatusBar/statusBarConfig';
 // import { ReleaseControlGardenHeader } from './Components/Garden/ReleaseControlGardenHeader';
@@ -39,6 +39,9 @@ import {
 export function setup(appApi: ClientApi): void {
     const responseAsync = async (signal?: AbortSignal): Promise<Response> => {
         const { FAM } = httpClient();
+        FAM.setBaseUrl('https://fam-synapse-api-dev.azurewebsites.net/');
+
+        FAM.setScopes(['api://cf9069c2-728f-4d16-9516-cf0031abd56e/access_as_user']);
 
         return await FAM.fetch(`/v0.1/procosys/pipetest/JCA`, { signal: signal });
     };
@@ -91,7 +94,32 @@ export function setup(appApi: ClientApi): void {
                         </StepFilterContainer>
                     );
                 },
+                sort: (values) => {
+                    values.sort((a, b) => {
+                        const map = new Map<string, number>();
+
+                        map.set('unknown', 0);
+                        map.set('pressuretest', 1);
+                        map.set('chemicalcleaning', 2);
+                        map.set('hotoilflushing', 3);
+                        map.set('bolttensioning', 4);
+                        map.set('painting', 5);
+                        map.set('a-test', 6);
+                        map.set('insulation', 7);
+                        map.set('boxInsulation', 8);
+                        map.set('b-test', 9);
+                        map.set('marking', 10);
+                        map.set('complete', 11);
+
+                        if (typeof a !== 'string') return 0;
+                        if (typeof b !== 'string') return 0;
+
+                        return (map.get(a.toLowerCase()) ?? -0) - (map.get(b.toLowerCase()) ?? -0);
+                    });
+                    return values;
+                },
             },
+
             {
                 name: 'System',
                 valueFormatter: ({ name }) => name.substring(0, 2),
