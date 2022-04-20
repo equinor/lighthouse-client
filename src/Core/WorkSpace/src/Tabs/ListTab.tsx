d1c0a78717a102c9df9d0396bbfdf10a62475bde
Gustav-Eikaas <guei@equinor.com>
2022-04-20 13:48:04 +0200

    :)

--- src/Core/WorkSpace/src/Tabs/ListTab.tsx
+++ src/Core/WorkSpace/src/Tabs/ListTab.tsx
@@ -5,7 +5,7 @@ import { useElementData } from '../../../../packages/Utils/Hooks/useElementData'
 import { useDataContext } from '../Context/DataProvider';
 
 const Wrapper = styled.section`
-    padding-left: 32px;
+    margin-left: 32px;
     padding-top: 32px;
 `;
 
@@ -42,7 +42,7 @@ export const ListTab = (): JSX.Element => {
                     groupByFn: defaultGroupByFn,
                     onSelect: tableOptions?.onSelect,
                 }}
-                height={awaitableHeight - 70}
+                height={awaitableHeight - 70 - 32}
                 itemSize={tableOptions?.itemSize}
             />
         </Wrapper>
