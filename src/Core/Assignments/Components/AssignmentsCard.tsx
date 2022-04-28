99203fd894c0951eb19eab376101ab2a990022e8
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2022-04-28 16:06:51 +0200

    [Assignments] First draft (#424)
    * assignments first draft
    
    * refactor onclick
    
    * refactor
    
    * added unread notifications
    
    * unused imports
    
    * remove console log
    
    * fix unique key issue
    
    * refactor
    
    * disabled assignments in production
    
    * remove group by notifications

---
+++ src/Core/Assignments/Components/AssignmentsCard.tsx
@@ -0,0 +1,71 @@
+import { DateTime } from 'luxon';
+import { useNavigate } from 'react-router';
+import { ClickableIcon } from '../../../packages/Components/Icon';
+import { useLocationKey } from '../../../packages/Filter/Hooks/useLocationKey';
+import { Assignment } from '../Types/assignment';
+import {
+    DetailText,
+    DueDate,
+    LeftSection,
+    NotificationTitle,
+    RightSection,
+    Wrapper,
+} from './assignmentCard.styles';
+import { handleActionClick } from '../../../components/ActionCenter/handleActionClick';
+
+interface AssignmentCardProps {
+    assignment: Assignment;
+}
+
+export const AssignmentCard = ({ assignment }: AssignmentCardProps): JSX.Element => {
+    const isToday =
+        new Date().toLocaleDateString() === assignment.dueDate &&
+        new Date(assignment.dueDate).toLocaleDateString();
+
+    const navigate = useNavigate();
+    const currentLocation = useLocationKey();
+
+    return (
+        <Wrapper>
+            <LeftSection>
+                {/* TODO: resolve EDS colors */}
+                <svg
+                    width={15}
+                    height={15}
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle cx="6" cy="6" r="5.5" fill={'#B276B2'} />
+                </svg>
+                <DetailText>
+                    <NotificationTitle>{assignment.title}</NotificationTitle>
+                </DetailText>
+            </LeftSection>
+            <RightSection>
+                {assignment.dueDate && (
+                    <DueDate overdue={new Date() > new Date(assignment.dueDate)}>
+                        {isToday
+                            ? 'Due today'
+                            : `Due ${DateTime.fromJSDate(new Date(assignment.dueDate)).toRelative({
+                                unit: 'days',
+                            })}`}
+                    </DueDate>
+                )}
+            </RightSection>
+            <div>
+                <ClickableIcon
+                    name="chevron_right"
+                    onClick={() =>
+                        handleActionClick(
+                            assignment.sourceSystem.subSystem,
+                            assignment.sourceSystem.identifier,
+                            navigate,
+                            currentLocation
+                        )
+                    }
+                />
+            </div>
+        </Wrapper>
+    );
+};
