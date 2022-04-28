de237fd1c4d17ca1ab8b61fda94edf893501a0c4
Christopher Berge Hove <chbh@equinor.com>
2022-04-28 07:33:24 +0200

    bouild fix 1

--- src/apps/ScopeChangeRequest/Atoms/scopeChangeAtom.ts
+++ src/apps/ScopeChangeRequest/Atoms/scopeChangeAtom.ts
@@ -1,7 +1,8 @@
 import { Atom } from '@dbeining/react-atom';
-import { SidesheetApi } from '../../../packages/Sidesheet/Components/ResizableSidesheet';
+import { SidesheetApi } from '@equinor/sidesheet';
 import { ScopeChangeAccess } from '../hooks/queries/useScopeChangeAccess';
 import { ScopeChangeRequest } from '../types/scopeChangeRequest';
+
 
 export interface ScopeChangeAtom {
     request: ScopeChangeRequest;
