5f0140c4274ca81dba8676efce8148a5f17f5e7e
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2022-04-29 10:01:48 +0200

    [Assignments] Make assignments closer to sketches (#465)
    * Make assignments closer to sketches
    
    * Make it pretty
    
    * add assignments frontpage
    
    * remove comments
    
    * cleanup
    
    * fix error banners showing
    
    * fix padding
    
    * fix padding again
    
    * align text
    
    * ready for production

--- src/Core/Assignments/Components/assignmentsTab.styles.ts
+++ src/Core/Assignments/Components/assignmentsTab.styles.ts
@@ -9,15 +9,17 @@ export const Header = styled.div`
 export const ActiveOrigins = styled.div`
     display: flex;
     flex-direction: row;
-    gap: 1em;
+    gap: 0.5em;
     padding: 0em 1em;
 `;
 
 export const Assignments = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 1em;
-    padding: 1em 0em;
+    gap: 0em;
+    padding: 0em 1em;
+
+    height: 100%;
 `;
 
 export const Transition = styled.div`
--- src/components/ActionCenter/GroupedAssignments.tsx
+++ src/components/ActionCenter/GroupedAssignments.tsx
@@ -1,6 +1,11 @@
 import { Accordion } from '@equinor/eds-core-react';
+import { tokens } from '@equinor/eds-tokens';
+import { useNavigate } from 'react-router';
+import styled from 'styled-components';
 import { AssignmentCard } from '../../Core/Assignments/Components/AssignmentsCard';
 import { Assignment } from '../../Core/Assignments/Types/assignment';
+import { useLocationKey } from '../../packages/Filter/Hooks/useLocationKey';
+import { handleActionClick } from './handleActionClick';
 
 interface GroupedAssignmentsProps {
     assignments: Assignment[];
@@ -12,26 +17,49 @@ export const GroupedAssignments = ({
     assignments,
 }: GroupedAssignmentsProps): JSX.Element => {
     const capitalize = (name: string) => name.charAt(0).toUpperCase() + name.slice(1);
+    const navigate = useNavigate();
+    const currentLocation = useLocationKey();
 
     return (
         <Accordion>
             {activeAssignments.map((applicationTitle) => (
                 <Accordion.Item key={applicationTitle}>
-                    <Accordion.Header chevronPosition="right">
-                        {capitalize(applicationTitle)}
-                    </Accordion.Header>
+                    <Header chevronPosition="right">{capitalize(applicationTitle)}</Header>
                     {assignments &&
                         assignments
                             .filter(
                                 ({ sourceSystem }) => sourceSystem.subSystem === applicationTitle
                             )
                             .map((assignment) => (
-                                <Accordion.Panel key={assignment.id}>
+                                <Panel
+                                    onClick={() =>
+                                        handleActionClick(
+                                            assignment.sourceSystem.subSystem,
+                                            assignment.sourceSystem.identifier,
+                                            navigate,
+                                            currentLocation
+                                        )
+                                    }
+                                    key={assignment.id}
+                                >
                                     <AssignmentCard assignment={assignment} />
-                                </Accordion.Panel>
+                                </Panel>
                             ))}
                 </Accordion.Item>
             ))}
         </Accordion>
     );
 };
+
+const Panel = styled(Accordion.Panel)`
+    margin: 0;
+    padding: 0;
+    height: auto;
+    border: none;
+    min-height: auto;
+`;
+
+const Header = styled(Accordion.Header)`
+    border: none;
+    border-bottom: 1px ${tokens.colors.interactive.disabled__border.hex} solid;
+`;
