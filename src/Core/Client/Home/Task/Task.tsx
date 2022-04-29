3843ca08701f54fbadf5c408c0f6698b5578f682
Gustav-Eikaas <guei@equinor.com>
2022-04-29 09:53:15 +0200

    fix padding

--- src/Core/Client/Home/Task/Task.tsx
+++ src/Core/Client/Home/Task/Task.tsx
@@ -1,11 +1,14 @@
 import { Typography } from '@equinor/eds-core-react';
 import { AssignmentsTab } from '../../../Assignments/Components/AssignmentsTab';
+import { Header } from '../clientHomeStyles';
 import { Wrapper } from './TaskStyle';
 
 export const Task = (): JSX.Element => {
     return (
         <Wrapper>
-            <Typography variant="h5">Assignments</Typography>
+            <Header>
+                <Typography variant="h5">Assignments</Typography>
+            </Header>
 
             <AssignmentsTab />
         </Wrapper>
--- src/Core/Client/Home/Task/TaskStyle.ts
+++ src/Core/Client/Home/Task/TaskStyle.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.section`
     display: flex;
     flex-direction: column;
-    margin: 1rem;
+    margin: 1rem 0rem;
     flex: 1;
     height: calc(100% - 2rem);
     overflow: hidden;
@@ -12,4 +12,7 @@ export const ContentWrapper = styled.div`
     display: flex;
     margin: 1rem 0px;
     flex-direction: column;
+`;
+const Header = styled.div`
+    margin-left: 1rem;
 `;
