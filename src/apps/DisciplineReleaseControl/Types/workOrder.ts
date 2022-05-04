f7bec50fd40c6688a7fe42a56852c261605afdc3
Andreas-Tonnessen <47244521+Andreas-Tonnessen@users.noreply.github.com>
2022-05-04 14:47:03 +0200

    Feature/workorder tab (#517)
    * Location on pipetest (filter/column)
    
    * Work order tab
    
    * Refactor WO table

---
+++ src/apps/DisciplineReleaseControl/Types/workOrder.ts
@@ -0,0 +1,33 @@
+export interface WorkOrder {
+    sourceIdentity: string;
+    facility: string;
+    project: string;
+    workOrderNo: string;
+    title: string;
+    description: string;
+    plannedStartupDate: string;
+    actualStartupDate: string;
+    plannedCompletionDate: string;
+    actualCompletionDate: string;
+    commissioningPackageNo: string;
+    milestone: string;
+    category: string;
+    holdBy: string;
+    typeOfWork: string;
+    onshoreOffshore: string;
+    projectProgress: string;
+    workOrderType: string;
+    jobStatus: string;
+    materialStatus: string;
+    estimatedManHours: string;
+    expendedManHours: string;
+    remainingManHours: string;
+    earnedManHours: string;
+    subMilestone: string;
+    isVoided: true;
+    responsible: string;
+    location: string;
+    discipline: string;
+    createdDate: string;
+    updatedDate: string;
+}
