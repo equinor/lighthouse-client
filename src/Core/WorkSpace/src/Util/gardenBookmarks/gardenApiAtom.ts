2ffb4ff27227fb5786a1505a209d8ffc488dc2f4
Gustav-Eikaas <guei@equinor.com>
2022-05-04 14:34:12 +0200

    build fix

--- src/Core/WorkSpace/src/Util/gardenBookmarks/gardenApiAtom.ts
+++ src/Core/WorkSpace/src/Util/gardenBookmarks/gardenApiAtom.ts
@@ -1,4 +1,4 @@
 import { Atom } from '@dbeining/react-atom';
-import { GardenApi } from '../../../../../components/ParkView/Components/VirtualGarden/Container';
+import { GardenApi } from '../../../../../components/ParkView/Models/gardenApi';
 
 export const gardenApiAtom = Atom.of<GardenApi | null>(null);
