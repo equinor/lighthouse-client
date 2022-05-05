58eddba8d0c9d449286923fac88b55fd0c185dd3
Christopher Berge Hove <chbh@equinor.com>
2022-05-05 14:36:40 +0200

    scopte  and url from infra

--- src/Core/Client/Service/setupContext.ts
+++ src/Core/Client/Service/setupContext.ts
@@ -3,18 +3,13 @@ import { setSelectedFacility, setSelectedFusionContext } from '../Functions/Cont
 import { Facility, FusionContext } from '../Types/ClientContext';
 
 export async function setupContext(): Promise<void> {
-    const { fusion } = httpClient();
-    fusion.setBaseUrl(
-        isProduction()
-            ? 'https://pro-s-context-fprd.azurewebsites.net/'
-            : 'https://pro-s-context-ci.azurewebsites.net/'
-    );
+    const { fusionContext } = httpClient();
 
     const fusionContextId = isProduction()
         ? '65728fee-185d-4a0c-a91d-8e3f3781dad8'
         : '71db33bb-cb1b-42cf-b5bf-969c77e40931';
 
-    const response = await fusion.get(`contexts/${fusionContextId}`);
+    const response = await fusionContext.get(`contexts/${fusionContextId}`);
     updateContext(await response.json());
 }
 
