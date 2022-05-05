591ee5a4e3fa0c4988ac20611ff138589f2136d1
Joachim <joachim290@outlook.com>
2022-05-05 13:07:39 +0200

    chore: refactor and use react-query for gardens

---
+++ src/apps/MechanicalCompletion/components/McSidesheet/hooks/useMcResource.ts
@@ -0,0 +1,36 @@
+import { usePackageResource } from '@equinor/GardenUtils';
+import { isProduction, useHttpClient } from '@equinor/portal-client';
+import { useCallback } from 'react';
+import { McNcr, McPunchItem, McWorkOrder } from '../types';
+type McResourceTypeMap = {
+    ncr: McNcr;
+    'work-orders': McWorkOrder;
+    punch: McPunchItem;
+};
+export const useMcResource = <T extends keyof McResourceTypeMap>(
+    packageId: string,
+    packageType: T
+) => {
+    const { fusionDataproxy } = useHttpClient();
+    const contextId = isProduction()
+        ? '65728fee-185d-4a0c-a91d-8e3f3781dad8'
+        : '71db33bb-cb1b-42cf-b5bf-969c77e40931';
+    const fetch = useCallback(
+        async (id: string) => {
+            const result = await fusionDataproxy.fetch(
+                `api/contexts/${contextId}/mc-pkgs/${id}/${packageType}`
+            );
+
+            return JSON.parse(await result.text()) as McResourceTypeMap[T][];
+        },
+        [fusionDataproxy, contextId, packageType]
+    );
+
+    const resource = usePackageResource(packageType, packageId, fetch);
+
+    return {
+        data: resource.data,
+        isFetching: resource.isFetching,
+        error: resource.error,
+    };
+};
--- src/apps/WorkOrder/Garden/components/WorkOrderSidesheet/hooks/useMaterial.ts
+++ src/apps/WorkOrder/Garden/components/WorkOrderSidesheet/hooks/useMaterial.ts
@@ -1,26 +1,28 @@
-import { isProduction } from '@equinor/portal-client';
+import { usePackageResource } from '@equinor/GardenUtils';
+import { isProduction, useHttpClient } from '@equinor/portal-client';
 import { useCallback } from 'react';
-import { useHttpClient } from '../../../../../../Core/Client/Hooks';
 import { WorkOrderMaterial } from '../../../models';
-import { usePackageResource } from './usePackageResource';
 
 export const useMaterial = (
-    packageId: string | null
-): { material: WorkOrderMaterial[]; isFetching: boolean; error: Error | null } => {
+    packageId: string
+): { material: WorkOrderMaterial[] | undefined; isFetching: boolean; error: Error | null } => {
     const { fusionDataproxy } = useHttpClient();
     const contextId = isProduction()
         ? '65728fee-185d-4a0c-a91d-8e3f3781dad8'
         : '71db33bb-cb1b-42cf-b5bf-969c77e40931';
-    const fetch = useCallback(async (id: string) => {
-        const response = await fusionDataproxy.fetch(
-            `api/contexts/${contextId}/work-orders/${id}/material`
-        );
-        return JSON.parse(await response.text()) as WorkOrderMaterial[];
-    }, []);
-    const resource = usePackageResource<WorkOrderMaterial>(packageId, fetch);
+    const fetch = useCallback(
+        async (id: string) => {
+            const response = await fusionDataproxy.fetch(
+                `api/contexts/${contextId}/work-orders/${id}/material`
+            );
+            return JSON.parse(await response.text()) as WorkOrderMaterial[];
+        },
+        [fusionDataproxy, contextId]
+    );
+    const resource = usePackageResource<WorkOrderMaterial>('material', packageId, fetch);
 
     return {
-        material: resource.data.filter(
+        material: resource?.data?.filter(
             (wo, i, list) => i === list.findIndex((w) => w.itemNumber === wo.itemNumber)
         ),
         isFetching: resource.isFetching,
