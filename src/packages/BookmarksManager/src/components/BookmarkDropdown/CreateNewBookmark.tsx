12a8656b30071c1d02c1da4777d5e415277c75dc
Joachim <joachim290@outlook.com>
2022-05-06 11:00:51 +0200

    chore: refactor onclick functions

--- src/packages/BookmarksManager/src/components/BookmarkDropdown/CreateNewBookmark.tsx
+++ src/packages/BookmarksManager/src/components/BookmarkDropdown/CreateNewBookmark.tsx
@@ -1,5 +1,5 @@
 import { ClickableIcon } from '@equinor/lighthouse-components';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { bookmarkEvents } from '../../utils';
 import {
     CreateNewBookmarkWrapper,
@@ -16,52 +16,49 @@ export const CreateNewBookmark = ({ appKey, subSystem }: CreateNewBookmarkProps)
     const [isCreatingNew, setIsCreatingNew] = useState<boolean>(false);
     const [title, setTitle] = useState<string>('');
     const { saveBookmark } = bookmarkEvents;
+
+    const handleToCreateNew = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+        setIsCreatingNew(true);
+        e.stopPropagation();
+    };
+
+    const handleSave = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+        saveBookmark({
+            title,
+            appKey,
+            subSystem,
+        });
+        setIsCreatingNew(false);
+        setTitle('');
+        e.stopPropagation();
+    };
+
+    const handleOnClose = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+        setIsCreatingNew(false);
+        setTitle('');
+        e.stopPropagation();
+    };
     if (!isCreatingNew) {
         return (
             <CreateNewBookmarkWrapper>
                 <div>New saved bookmark</div>
-                <ClickableIcon
-                    name="add"
-                    onClick={(e) => {
-                        setIsCreatingNew(true);
-                        e.stopPropagation();
-                    }}
-                />
+                <ClickableIcon name="add" onClick={handleToCreateNew} />
             </CreateNewBookmarkWrapper>
         );
-    } else {
-        return (
-            <CreatingNewBookmarkWrapper>
-                <div>
-                    <TitleInput
-                        onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Bookmark title"
-                    />
-                </div>
-                <CreatingNewIcons>
-                    <ClickableIcon
-                        name="save"
-                        onClick={(e) => {
-                            saveBookmark({
-                                title,
-                                appKey,
-                                subSystem,
-                            });
-                            setIsCreatingNew(false);
-                            setTitle('');
-                            e.stopPropagation();
-                        }}
-                    />
-                    <ClickableIcon
-                        name="close"
-                        onClick={(e) => {
-                            setIsCreatingNew(false);
-                            setTitle('');
-                            e.stopPropagation();
-                        }}
-                    />
-                </CreatingNewIcons>
-            </CreatingNewBookmarkWrapper>
-        );
     }
+    return (
+        <CreatingNewBookmarkWrapper>
+            <div>
+                <TitleInput
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Bookmark title"
+                    autoFocus
+                />
+            </div>
+            <CreatingNewIcons>
+                <ClickableIcon name="save" onClick={handleSave} />
+                <ClickableIcon name="close" onClick={handleOnClose} />
+            </CreatingNewIcons>
+        </CreatingNewBookmarkWrapper>
+    );
 };
