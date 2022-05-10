c4c7ddee40a6d9da119cbbef1faafba9d53ce8d0
Joachim <joachim290@outlook.com>
2022-05-10 15:10:36 +0200

    fix: undefined check

--- src/packages/BookmarksManager/src/utils/isBookmarkResponse.ts
+++ src/packages/BookmarksManager/src/utils/isBookmarkResponse.ts
@@ -3,5 +3,5 @@ import { BookmarkErrorResponse, BookmarkResponse } from '../types';
 export const isBookmarkResponse = (
     data: BookmarkResponse[] | BookmarkErrorResponse
 ): data is BookmarkResponse[] => {
-    return Array.isArray(data) && (data[0] as BookmarkResponse).id !== undefined;
+    return Array.isArray(data) && (data[0] as BookmarkResponse)?.id !== undefined;
 };
