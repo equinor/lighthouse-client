f54a8b745e82792065cc7eea9841a12f149e4367
Christopher Berge Hove <chbh@equinor.com>
2022-05-11 13:50:13 +0200

    [Sidesheet] - widgets and functions  (#576)
    * lighthouse utils rename
    
    * Widgets
    
    * Functions
    
    * Providers
    
    * sidesheet test
    
    * remove Functions form window
    
    * widget and function updates
    
    * refactor 1
    
    * open side sheet
    
    * sidesheet appName
    
    * fixing build
    
    * pr fix ;)

--- src/packages/Sidesheet/State/sidesheetState.ts
+++ src/packages/Sidesheet/State/sidesheetState.ts
@@ -3,10 +3,10 @@ import React from 'react';
 export interface SidesheetState<T> {
     SidesheetComponent?: React.FC<T>;
     props?: T;
-    isPinned?: true;
     isMinimized: boolean;
     minWidth: number;
     defaultWidth: number;
     width: number;
     appName?: string;
+    color: string;
 }
