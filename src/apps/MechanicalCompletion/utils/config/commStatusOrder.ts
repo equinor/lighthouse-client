e15462c903d7b7e0057bfadbb04733ebe98c1baf
Joachim Senland <joachim290@gmail.com>
2022-05-16 15:17:30 +0200

    [Mechanical Completion] - chore: rename shipped to sent (#609)

--- src/apps/MechanicalCompletion/utils/config/commStatusOrder.ts
+++ src/apps/MechanicalCompletion/utils/config/commStatusOrder.ts
@@ -2,13 +2,13 @@ import { CommissioningStatus } from '../../types';
 
 export const commissioningStatusOrder: Record<CommissioningStatus, number> = {
     'RFOC Accepted': 11,
-    'RFOC Shipped': 10,
+    'RFOC Sent': 10,
     'RFOC Rejected': 9,
     'TAC Accepted': 8,
-    'TAC Shipped': 7,
+    'TAC Sent': 7,
     'TAC Rejected': 6,
     'RFCC Accepted': 5,
-    'RFCC Shipped': 4,
+    'RFCC Sent': 4,
     'RFCC Rejected': 3,
     'Punch status accepted': 2,
     'Contractor final punch': 1,
