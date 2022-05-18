8f53f1bd8c13e1e482237850e2f831c54b6913f8
Gustav-Eikaas <guei@equinor.com>
2022-05-18 12:25:57 +0200

    Warning when switching sidesheets

--- src/packages/Sidesheet/Hooks/useInternalSidesheetFunction.tsx
+++ src/packages/Sidesheet/Hooks/useInternalSidesheetFunction.tsx
@@ -1,23 +1,25 @@
-import { getSidesheetContext } from '../context/sidesheetContext';
+import { deref, swap } from '@dbeining/react-atom';
+import { spawnConfirmationDialog } from '../../../Core/ConfirmationDialog/Functions/spawnConfirmationDialog';
+import { getSidesheetContext, SidesheetCoreContext } from '../context/sidesheetContext';
 import { dispatch } from '../State/actions';
 import { SidesheetState } from '../State/sidesheetState';
 
 export type ToggleFunction = (prev: boolean) => boolean;
 interface InternalSidesheetFunctions {
     closeSidesheet(): void;
-    togglePinned(): void;
     setWidth(width: number): void;
     setIsMinimized(isMinimized: ToggleFunction | boolean): void;
+    setHasUnsavedChanges(hasUnsaved: boolean): void;
 }
 
 export function useInternalSidesheetFunction(): InternalSidesheetFunctions {
-    function togglePinned(): void {
-        dispatch(getSidesheetContext(), (currentState: SidesheetState<any>) => {
-            return {
-                ...currentState,
-            };
-        });
+    function setHasUnsavedChanges(hasUnsaved: boolean): void {
+        dispatch(getSidesheetContext(), (s: SidesheetState<any>) => ({
+            ...s,
+            hasUnsavedChanges: hasUnsaved,
+        }));
     }
+
     function setWidth(width: number): void {
         dispatch(getSidesheetContext(), (currentState: SidesheetState<any>) => {
             return {
@@ -39,6 +41,18 @@ export function useInternalSidesheetFunction(): InternalSidesheetFunctions {
     }
 
     function closeSidesheet(): void {
+        const state = deref(SidesheetCoreContext);
+        if (state.hasUnsavedChanges) {
+            spawnConfirmationDialog(
+                'Unsaved changes, are you sure you want to abandon your changes',
+                'Warning!',
+                () => {
+                    swap(SidesheetCoreContext, (s) => ({ ...s, hasUnsavedChanges: false }));
+                    closeSidesheet();
+                }
+            );
+            return;
+        }
         //HACK: should somehow be handled from the workspace
         history.pushState('', document.title, window.location.pathname);
         dispatch(getSidesheetContext(), (currentState: SidesheetState<any>) => {
@@ -52,7 +66,7 @@ export function useInternalSidesheetFunction(): InternalSidesheetFunctions {
     }
 
     return {
-        togglePinned,
+        setHasUnsavedChanges,
         closeSidesheet,
         setWidth,
         setIsMinimized,
