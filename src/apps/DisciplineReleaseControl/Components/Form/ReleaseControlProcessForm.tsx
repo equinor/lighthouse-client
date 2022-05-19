4f381119c078781dc29d04f1523ded1a68dc47dd
Andreas Tunheim Tønnessen <andreas.tonnessen@bouvet.no>
2022-05-19 11:54:01 +0200

    Release control workflow with step editing

--- src/apps/DisciplineReleaseControl/Components/Form/ReleaseControlProcessForm.tsx
+++ src/apps/DisciplineReleaseControl/Components/Form/ReleaseControlProcessForm.tsx
@@ -1,12 +1,15 @@
-import { Button } from '@equinor/eds-core-react';
+import { Button, SingleSelect } from '@equinor/eds-core-react';
 import { tokens } from '@equinor/eds-tokens';
 import styled from 'styled-components';
 
 import { DRCFormAtomApi } from '../../Atoms/formAtomApi';
 import { TitleInput, DescriptionInput, PlannedStartDateInput, ReferencesInput } from './Inputs';
 import { FlexColumn, FormWrapper } from './releaseControlProcessForm.styles';
+import { WorkflowCustomEditor } from './WorkflowEditor/WorkflowCustomEditor';
+import { getNewWorkflowSteps } from './WorkflowEditor/WorkflowEditorHelpers';
 
 export const ReleaseControlProcessForm = (): JSX.Element => {
+    const { updateAtom } = DRCFormAtomApi;
     return (
         <>
             <div>
@@ -16,17 +19,35 @@ export const ReleaseControlProcessForm = (): JSX.Element => {
                         <TitleInput />
                         <DescriptionInput />
                         <PlannedStartDateInput />
+                        <ReferencesInput />
                     </FlexColumn>
                     <FlexColumn>
                         Workflow
                         <div style={{ fontWeight: 400, fontSize: '16px' }}>
                             Select a workflow to start with or create a complete custom flow.
                         </div>
-                        <div>Insert custom select bar here...</div>
-                    </FlexColumn>
-
-                    <FlexColumn>
-                        <ReferencesInput />
+                        <SelectionRow>
+                            <SingleSelect
+                                items={predefinedWorkflows}
+                                label="Workflow"
+                                placeholder="Select predefined workflow"
+                                size={30}
+                                handleSelectedItemChange={() => {
+                                    return null;
+                                }}
+                            />
+                            <Button
+                                style={{ width: '100px', marginLeft: '20px', marginTop: '16px' }}
+                                onClick={() =>
+                                    updateAtom({
+                                        steps: getNewWorkflowSteps(),
+                                    })
+                                }
+                            >
+                                New flow
+                            </Button>
+                        </SelectionRow>
+                        <WorkflowCustomEditor />
                     </FlexColumn>
                 </FormWrapper>
                 <SubmitButtonBar />
@@ -69,3 +90,15 @@ export const ActionBar = styled.div`
     border: 1px solid ${tokens.colors.interactive.disabled__border.hex};
     background-color: white;
 `;
+
+export const SelectionRow = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const predefinedWorkflows = [
+    'Commissioning - With HT',
+    'Commissioning - No HT',
+    'Operation - With HT',
+    'Operation - No HT',
+];
