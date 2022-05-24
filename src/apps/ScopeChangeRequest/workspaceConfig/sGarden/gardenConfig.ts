98474e2e4ab78ea2b26d9f4bcc3833dd2c4c5207
Gustav-Eikaas <guei@equinor.com>
2022-05-24 08:10:25 +0200

    Scopechange garden config

--- src/apps/ScopeChangeRequest/workspaceConfig/sGarden/gardenConfig.ts
+++ src/apps/ScopeChangeRequest/workspaceConfig/sGarden/gardenConfig.ts
@@ -1,5 +1,6 @@
 import { GardenOptions } from '../../../../components/ParkView/Models/gardenOptions';
 import { ScopeChangeRequest } from '../../types/scopeChangeRequest';
+import { ScopechangeGardenItem } from './CustomItemView';
 
 const workflowStatusMap = new Map([
     ['Initiate', 1],
@@ -14,9 +15,12 @@ const workflowStatusMap = new Map([
 ]);
 
 export const gardenConfig: GardenOptions<ScopeChangeRequest> = {
-    gardenKey: 'state',
+    gardenKey: 'Status' as keyof ScopeChangeRequest,
     itemKey: 'sequenceNumber',
     type: 'normal',
+    customViews: {
+        customItemView: ScopechangeGardenItem,
+    },
     fieldSettings: {
         CurrentStep: {
             getKey: ({ currentWorkflowStep }) => currentWorkflowStep?.name ?? '(Blank)',
