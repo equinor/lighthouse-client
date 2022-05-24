09cb9102fccbff4a755223056c5b2fe552e22d9e
Andreas Tunheim Tønnessen <andreas.tonnessen@bouvet.no>
2022-05-24 15:54:08 +0200

    Data creator config (comment in when access control is OK)

--- src/apps/ReleaseControl/workspaceConfig/DataCreator/dataCreatorConfig.tsx
+++ src/apps/ReleaseControl/workspaceConfig/DataCreator/dataCreatorConfig.tsx
@@ -5,6 +5,13 @@ import { DisciplineReleaseControlFactoryComponent } from '../../components/Facto
 export const dataCreatorConfig: FactoryOptions = {
     title: 'Create release control workflow',
     accessCheck: () => Promise.resolve(true),
+    // accessCheck: async (): Promise<boolean> => {
+    //     const { scopeChange } = httpClient();
+    //     const check = () => scopeChange.fetch('api/releasecontrol', { method: 'OPTIONS' });
+    //     return await (
+    //         await checkOptionsRequest(check)
+    //     ).canPost;
+    // },
     onClick: () => {
         openSidesheet(DisciplineReleaseControlFactoryComponent, undefined, 'piping-and-ht');
     },
