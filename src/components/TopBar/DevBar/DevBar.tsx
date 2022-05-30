f872cb5f2f65095737756b5efb93fa16a650366a
Gustav-Eikaas <guei@equinor.com>
2022-05-30 11:06:16 +0200

    add version to banner

--- src/components/TopBar/DevBar/DevBar.tsx
+++ src/components/TopBar/DevBar/DevBar.tsx
@@ -1,4 +1,5 @@
 import { BetaTag } from './DevBarStyles';
+import text from '../../../../package.json';
 
 interface DevBarProps {
     env: string;
@@ -10,6 +11,7 @@ export const DevBar = ({ env }: DevBarProps): JSX.Element | null => {
             <BetaTag>
                 <b>UNDER DEVELOPMENT - {env.toUpperCase()} </b>
                 <p>This site contains test data.</p>
+                <p>V.{text.version}</p>
             </BetaTag>
         );
 
