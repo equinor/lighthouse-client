e7ef23d6fbbedef092f0d1c81c5f00b21b0e65ec
Gustav-Eikaas <guei@equinor.com>
2022-05-30 13:35:56 +0200

    navigation breadcrumbs

---
+++ src/components/TopBar/BreadCrumbs/Breadcrumbs.tsx
@@ -0,0 +1,49 @@
+import { Breadcrumbs } from '@equinor/eds-core-react';
+import { useLocation } from 'react-router';
+import styled from 'styled-components';
+
+import { readClientRegistry } from '../../../Core/Client/Functions';
+
+export const LocationBreadCrumbs = (): JSX.Element => {
+    const location = useLocation();
+
+    const createBreadCrumbs = () => {
+        const paths = location.pathname.split('/').filter((s) => s !== '');
+        if (paths.length < 2) {
+            return [];
+        }
+
+        const clientRegistry = readClientRegistry();
+        const groupName: string | undefined = clientRegistry.appGroups[paths[0]].name;
+        const appName = clientRegistry.apps.find((s) => s.shortName === paths[1])?.title;
+        const tab = paths[2];
+
+        const crumbs: string[] = [];
+        if (groupName) crumbs.push(groupName);
+        if (appName) crumbs.push(appName);
+        if (tab) crumbs.push(tab[0].toUpperCase() + tab.substring(1));
+
+        return crumbs;
+    };
+
+    return (
+        <>
+            {createBreadCrumbs().map((s) => (
+                <>
+                    <BreadcrumbStyle>/</BreadcrumbStyle>
+                    <Breadcrumbs key={s}>
+                        <BreadcrumbStyle>{s}</BreadcrumbStyle>
+                    </Breadcrumbs>
+                </>
+            ))}
+        </>
+    );
+};
+
+const BreadcrumbStyle = styled.div`
+    font-family: Equinor;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 16px;
+    text-align: left;
+`;
