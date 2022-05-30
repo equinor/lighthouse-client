3960d86d49801867beb8fa52ebea40898533ebaa
Christopher Berge Hove <chbh@equinor.com>
2022-05-30 14:25:01 +0200

    onhashchange chech

--- src/packages/Sidesheet/Components/SidesheetLoader.tsx
+++ src/packages/Sidesheet/Components/SidesheetLoader.tsx
@@ -1,10 +1,10 @@
 import Widget from '@equinor/lighthouse-widgets';
 import { PropsWithChildren, useEffect } from 'react';
+import { useLocation } from 'react-router';
 import { openSidesheetById } from '../Functions';
 import { handleExtractHashUrl } from '../Utils/urlHandler';
 
-const mountSidesheetFromUrl = async () => {
-    const { widgetId, itemId } = handleExtractHashUrl();
+const mountSidesheetFromUrl = async (widgetId?: string, itemId?: string) => {
     if (widgetId) {
         try {
             const manifest = await Widget.getWidgetManifest(widgetId);
@@ -15,16 +15,33 @@ const mountSidesheetFromUrl = async () => {
     }
 };
 
+const handleHashchange = () => {
+    const { widgetId, itemId } = handleExtractHashUrl();
+    mountSidesheetFromUrl(widgetId, itemId);
+};
+
 export const SidesheetLoader = ({ children }: PropsWithChildren<any>): JSX.Element => {
+    const location = useLocation();
     useEffect(() => {
         if (window.location.hash.length > 0) {
             mountSidesheetFromUrl();
         }
-        window.addEventListener('hashchange', mountSidesheetFromUrl, false);
+        if ('onhashchange' in window) {
+            window.addEventListener('hashchange', handleHashchange, false);
+        }
         return () => {
-            window.removeEventListener('hashchange', mountSidesheetFromUrl, false);
+            if ('onhashchange' in window) {
+                window.removeEventListener('hashchange', handleHashchange, false);
+            }
         };
     }, []);
+
+    useEffect(() => {
+        if ('onhashchange' in window) {
+            const values = location.hash.replace('#', '').split('/');
+            mountSidesheetFromUrl(values[0], values[1]);
+        }
+    }, [location]);
 
     return <>{children}</>;
 };
