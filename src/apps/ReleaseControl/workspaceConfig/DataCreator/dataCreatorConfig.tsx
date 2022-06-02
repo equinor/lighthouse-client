f244dfc4035955f4d4f2732c2e198de7813ce7e9
Andreas Tunheim Tønnessen <andreas.tonnessen@bouvet.no>
2022-06-02 14:34:09 +0200

    Access check for release control in dataCreatorConfig

--- src/apps/ReleaseControl/workspaceConfig/DataCreator/dataCreatorConfig.tsx
+++ src/apps/ReleaseControl/workspaceConfig/DataCreator/dataCreatorConfig.tsx
@@ -1,17 +1,18 @@
+import { httpClient } from '@equinor/lighthouse-portal-client';
 import { openSidesheet } from '@equinor/sidesheet';
 import { FactoryOptions } from '@equinor/WorkSpace';
+import { checkOptionsRequest } from '../../api/releaseControl/Access/optionsRequestChecker';
 import { DisciplineReleaseControlFactoryComponent } from '../../components/Factory/FactoryComponent';
 
 export const dataCreatorConfig: FactoryOptions = {
     title: 'Create release control workflow',
-    accessCheck: () => Promise.resolve(true),
-    // accessCheck: async (): Promise<boolean> => {
-    //     const { scopeChange } = httpClient();
-    //     const check = () => scopeChange.fetch('api/releasecontrol', { method: 'OPTIONS' });
-    //     return await (
-    //         await checkOptionsRequest(check)
-    //     ).canPost;
-    // },
+    accessCheck: async (): Promise<boolean> => {
+        const { scopeChange } = httpClient();
+        const check = () => scopeChange.fetch('api/releasecontrol', { method: 'OPTIONS' });
+        return await (
+            await checkOptionsRequest(check)
+        ).canPost;
+    },
     onClick: () => {
         openSidesheet(DisciplineReleaseControlFactoryComponent, undefined, 'piping-and-ht');
     },
