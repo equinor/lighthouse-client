3f40a26a2a9cb159de8fcce91caab2808f04c0a6
Christopher Berge Hove <chbh@equinor.com>
2022-06-08 07:40:56 +0200

    Feature/conditionally show electrical tags (#720)
    * electro view
    
    * tag selection and electro tags
    
    * loading and pr fix

---
+++ src/apps/DisciplineReleaseControl/Components/3D/3dView.tsx
@@ -0,0 +1,86 @@
+import { tokens } from '@equinor/eds-tokens';
+import { Icon } from '@equinor/lighthouse-components';
+import { useModelViewerContext, Viewer } from '@equinor/lighthouse-model-viewer';
+import { useFacility } from '@equinor/lighthouse-portal-client';
+import { useEffect, useMemo, useState } from 'react';
+import { useQuery } from 'react-query';
+import { EleNetwork } from '../../Types/eleNetwork';
+import { Pipetest } from '../../Types/pipetest';
+import { getEleNetworks } from '../Electro/getEleNetworks';
+import { MessageWrapper, ThreeDModel } from './3dViewStyles';
+import { ElectroIcon } from './icons/ElectroIcon';
+
+interface I3DViewProp {
+    pipetest: Pipetest;
+}
+
+export const ThreeDView = ({ pipetest }: I3DViewProp): JSX.Element => {
+    const { echoPlantId } = useFacility();
+    const [isElectro, setIsElectro] = useState(false);
+
+    const circuitStarterTagNosArray = pipetest.circuits.map((c) => c.circuitAndStarterTagNo);
+    const circuitStarterTagNos = circuitStarterTagNosArray.toString();
+
+    const { data } = useQuery([circuitStarterTagNos], () => getEleNetworks(circuitStarterTagNos), {
+        staleTime: Infinity,
+        cacheTime: Infinity,
+    });
+
+    const electroTags = useMemo(
+        () => data?.map((eN: EleNetwork) => eN.checkLists.map((cl) => cl.tagNo)).flat() || [],
+        [data]
+    );
+
+    const { selectTags } = useModelViewerContext();
+
+    useEffect(() => {
+        selectTags(pipetest.lineNos, { clearSelection: true });
+    }, [pipetest.name]);
+
+    if (pipetest.lineNos.length === 0 && electroTags.length === 0)
+        return (
+            <MessageWrapper>
+                <Icon
+                    name={'warning_outlined'}
+                    color={tokens.colors.interactive.warning__resting.rgba}
+                    size={48}
+                />
+                <h2>No tags found for this pipetest</h2>
+            </MessageWrapper>
+        );
+
+    return (
+        <ThreeDModel>
+            <Viewer
+                echoPlantId={echoPlantId}
+                padding={1}
+                selectionActions={[
+                    {
+                        title: 'Toggle Electro',
+                        disabled: electroTags.length === 0,
+                        icon: () => (
+                            <ElectroIcon
+                                color={
+                                    isElectro
+                                        ? tokens.colors.interactive.primary__resting.rgba
+                                        : tokens.colors.text.static_icons__default.rgba
+                                }
+                            />
+                        ),
+                        onClick: () => {
+                            setIsElectro((isElectro) => {
+                                isElectro = !isElectro;
+                                selectTags(
+                                    isElectro
+                                        ? [...electroTags, ...pipetest.lineNos]
+                                        : pipetest.lineNos
+                                );
+                                return isElectro;
+                            });
+                        },
+                    },
+                ]}
+            />
+        </ThreeDModel>
+    );
+};
