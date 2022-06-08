6712f014cace34f0a5427d1d7f9bc51fe5effc4b
Joachim Senland <joachim290@gmail.com>
2022-06-08 14:37:05 +0200

    [Query & Checklist] - chore: add pages to query and checklist (#722)

--- src/apps/PowerBI/QueryReport.tsx
+++
@@ -1,7 +0,0 @@
-import { PowerBI } from '@equinor/lighthouse-powerbi';
-
-export const QueryReport = (): JSX.Element => {
-    const reportUri = 'pp-query-analytics';
-
-    return <PowerBI reportUri={reportUri} />;
-};
