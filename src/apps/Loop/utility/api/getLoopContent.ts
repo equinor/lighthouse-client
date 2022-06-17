88436667d7c76be166df65740ac19f5b01ae7276
Joachim <joachim290@outlook.com>
2022-06-17 12:09:40 +0200

    chore: update fam request type

--- src/apps/Loop/utility/api/getLoopContent.ts
+++ src/apps/Loop/utility/api/getLoopContent.ts
@@ -1,7 +1,8 @@
 import { httpClient } from '@equinor/lighthouse-portal-client';
 import { LoopContent } from '../../types';
+import { FamRequest } from '../helpers/fam';
 
-export const getLoopContent = async (famFilter: any): Promise<LoopContent[]> => {
+export const getLoopContent = async (famFilter: FamRequest): Promise<LoopContent[]> => {
     const { FAM } = httpClient();
     const res = await FAM.post(`v0.1/dynamic/completion/custom_loopcontent/JCA`, {
         body: JSON.stringify(famFilter),
