c06aa65fbad37190a4818ebcb75c84395f31a5c2
Joachim <joachim290@outlook.com>
2022-06-17 13:31:46 +0200

    feat: workorder tab in sidesheet

--- src/apps/Loop/utility/api/getLoopContent.ts
+++ src/apps/Loop/utility/api/getLoopContent.ts
@@ -1,11 +1,29 @@
 import { httpClient } from '@equinor/lighthouse-portal-client';
 import { LoopContent } from '../../types';
 import { FamRequest } from '../helpers/fam';
-
-export const getLoopContent = async (famFilter: FamRequest): Promise<LoopContent[]> => {
+export const columnNames = [
+    'Facility',
+    'LoopId',
+    'ContentId',
+    'Register',
+    'MechanicalCompletionStatus',
+    'MechanicalCompletionStatusOrder',
+    'MechanicalCompletionPackageNo',
+    'CommissioningPackageNo',
+    'Description',
+    'PlannedCompletionDate',
+    'ActualCompletionDate',
+    'RemainingManHours',
+    'ChecklistID',
+];
+export const getLoopContent = async (
+    famFilter: FamRequest,
+    signal?: AbortSignal
+): Promise<LoopContent[]> => {
     const { FAM } = httpClient();
     const res = await FAM.post(`v0.1/dynamic/completion/custom_loopcontent/JCA`, {
         body: JSON.stringify(famFilter),
+        signal,
     });
 
     if (!res.ok) {
