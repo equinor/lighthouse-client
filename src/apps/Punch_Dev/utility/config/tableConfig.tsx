8acf52b869aec0446a5322f1a8b8d8c686d80f31
Joachim <joachim290@outlook.com>
2022-06-20 13:14:36 +0200

    chore: change punch view

--- src/apps/Punch_Dev/utility/config/tableConfig.tsx
+++ src/apps/Punch_Dev/utility/config/tableConfig.tsx
@@ -4,6 +4,7 @@ import { Punch } from '../../types/punch';
 export const tableConfig: TableOptions<Punch> = {
     objectIdentifierKey: 'punchItemNo',
     preventAutoGenerateColumns: true,
+
     customColumns: [
         {
             id: 'punchItemNo',
@@ -13,14 +14,7 @@ export const tableConfig: TableOptions<Punch> = {
             aggregate: 'count',
             width: 100,
         },
-        {
-            id: 'checklistId',
-            Header: 'Checklist',
-            accessor: (pkg) => pkg.checklistId,
-            Aggregated: () => null,
-            aggregate: 'count',
-            width: 100,
-        },
+
         {
             id: 'description',
             Header: 'Description',
@@ -35,7 +29,61 @@ export const tableConfig: TableOptions<Punch> = {
             accessor: (pkg) => pkg.punchItemCategory,
             Aggregated: () => null,
             aggregate: 'count',
-            width: 300,
+            width: 100,
+        },
+        {
+            id: 'c01ForecastDate',
+            Header: 'RFC Forecast/Planned',
+            accessor: (pkg) => pkg.c01ForecastDate ?? pkg.c01PlannedDate,
+            Aggregated: () => null,
+            aggregate: 'count',
+            width: 150,
+            Cell: (cellProps) => {
+                return cellProps.value ? new Date(cellProps.value).toLocaleDateString() : '';
+            },
+        },
+        {
+            id: 'c07ForecastDate',
+            Header: 'RFO Forecast/Planned',
+            accessor: (pkg) => pkg.c07ForecastDate ?? pkg.c07PlannedDate,
+            Aggregated: () => null,
+            aggregate: 'count',
+            width: 150,
+            Cell: (cellProps) => {
+                return cellProps.value ? new Date(cellProps.value).toLocaleDateString() : '';
+            },
+        },
+        {
+            id: 'formularType',
+            Header: 'Form type',
+            accessor: (pkg) => pkg.formularType,
+            Aggregated: () => null,
+            aggregate: 'count',
+            width: 100,
+        },
+        {
+            id: 'priority1',
+            Header: 'Priority',
+            accessor: (pkg) => pkg.priority1,
+            Aggregated: () => null,
+            aggregate: 'count',
+            width: 100,
+        },
+        {
+            id: 'punchListSorting',
+            Header: 'PL Sorting',
+            accessor: (pkg) => pkg.punchListSorting,
+            Aggregated: () => null,
+            aggregate: 'count',
+            width: 100,
+        },
+        {
+            id: 'punchListType',
+            Header: 'PL Type',
+            accessor: (pkg) => pkg.punchListType,
+            Aggregated: () => null,
+            aggregate: 'count',
+            width: 100,
         },
         {
             id: 'estimate',
@@ -44,6 +92,22 @@ export const tableConfig: TableOptions<Punch> = {
             Aggregated: () => null,
             aggregate: 'count',
             width: 100,
+        },
+        {
+            id: 'raisedByOrganization',
+            Header: 'Raised by org',
+            accessor: (pkg) => pkg.raisedByOrganization,
+            width: 150,
+            Aggregated: () => null,
+            aggregate: 'count',
+        },
+        {
+            id: 'clearingByOrganization',
+            Header: 'Clearing by org',
+            accessor: (pkg) => pkg.clearingByOrganization,
+            width: 150,
+            Aggregated: () => null,
+            aggregate: 'count',
         },
     ],
 };
