de27c9a6f0e542a973fd4b29bdc0ccd5cd2cbcab
Joachim <joachim290@outlook.com>
2022-06-20 10:52:47 +0200

    feat: highlight current week column

--- src/apps/Loop/utility/config/gardenConfig.ts
+++ src/apps/Loop/utility/config/gardenConfig.ts
@@ -1,6 +1,5 @@
-import { sortByNumber } from '@equinor/GardenUtils';
+import { getYearAndWeekFromDate, sortByNumber } from '@equinor/GardenUtils';
 import { FieldSettings, GardenOptions } from '@equinor/ParkView';
-import { LoopGroupBySelect } from '../../components';
 import { CustomGroupByKeys } from '../../types';
 import { Loop } from '../../types/loop';
 import { getDateKey } from '../helpers/getGroupByKey';
@@ -27,12 +26,35 @@ const customGroupByKeys: CustomGroupByKeys = {
     plannedForecast: 'Planned',
     weeklyDaily: 'Weekly',
 };
+
+const getGroupBy = (groupBy: keyof Loop & ExtendedGardenFields): keyof Loop => {
+    switch (groupBy) {
+        case 'RFC':
+            return 'rfC_Planned_Forecast_Date';
+        case 'RFO':
+            return 'rfO_Planned_Forecast_Date';
+        default:
+            return groupBy;
+    }
+};
+const getHighlightedColumn = (groupByKey) => {
+    const groupBy = getGroupBy(groupByKey as keyof Loop & ExtendedGardenFields);
+    switch (groupBy) {
+        case 'rfC_Planned_Forecast_Date':
+        case 'rfO_Planned_Forecast_Date':
+            return getYearAndWeekFromDate(new Date());
+
+        default:
+            return undefined;
+    }
+};
 export const gardenConfig: GardenOptions<Loop> = {
     gardenKey: 'RFC' as keyof Loop,
     itemKey: 'tagNo',
     objectIdentifier: 'checklistId',
     customGroupByKeys,
     fieldSettings,
+    highlightColumn: getHighlightedColumn,
     // customViews: {
     //     customGroupByView: LoopGroupBySelect,
     // },
