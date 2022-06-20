bcc51c56e9b35c2f8a63e46b08765eda94fd77ba
Gustav-Eikaas <guei@equinor.com>
2022-06-20 19:11:26 +0200

    Search implemented

--- src/modules/powerBI/src/Components/Filter/FilterTypeSelector/FilterTypeSelector.tsx
+++ src/modules/powerBI/src/Components/Filter/FilterTypeSelector/FilterTypeSelector.tsx
@@ -1,6 +1,9 @@
-import { Icon } from '@equinor/eds-core-react';
+import { Icon, Search } from '@equinor/eds-core-react';
 import { tokens } from '@equinor/eds-tokens';
+import { Case } from '@equinor/JSX-Switch';
+import { useState } from 'react';
 import styled from 'styled-components';
+import { Switch } from '../../../../../../components/JSXSwitch/Components/Switch';
 
 interface FilterTypeSelectorProps {
     visibleFilters: string[];
@@ -12,14 +15,43 @@ export function FilterTypeSelector({
     setVisibleFilters,
     visibleFilters,
 }: FilterTypeSelectorProps): JSX.Element {
+    const [searchText, setSearchText] = useState('');
+    const [isSearchActive, setIsSearchActive] = useState(false);
+    const handleInput = (e) => setSearchText(e.target.value.toString().toLowerCase());
+
+    const handleEnterPress = () => {
+        const matches = getFiltersMatchingSearchText();
+        if (!matches.length) return;
+        setVisibleFilters(matches);
+    };
+
+    const getFiltersMatchingSearchText = () =>
+        allFilters.filter((name) => !searchText || name.toLowerCase().startsWith(searchText));
+
     return (
         <FilterTypeSelectorWrapper>
             <Header>
-                <div>Filter types</div>
-                <Icon color={tokens.colors.interactive.primary__resting.hex} name="search" />
+                <Switch>
+                    <Case when={isSearchActive}>
+                        <Search
+                            onInput={handleInput}
+                            value={searchText}
+                            onKeyPress={(e) => e.key === 'Enter' && handleEnterPress()}
+                            placeholder="Search for filters.."
+                        />
+                    </Case>
+                    <Case when={!isSearchActive}>
+                        <div>Filter types</div>
+                        <Icon
+                            onClick={() => setIsSearchActive(true)}
+                            color={tokens.colors.interactive.primary__resting.hex}
+                            name="search"
+                        />
+                    </Case>
+                </Switch>
             </Header>
             <Options>
-                {allFilters.map((name) => {
+                {getFiltersMatchingSearchText().map((name) => {
                     const isSelected = visibleFilters.includes(name);
                     return (
                         <FilterTypeOption
