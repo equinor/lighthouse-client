6818ae7b35a4ea383e6de097ac0684256da20658
Gustav-Eikaas <guei@equinor.com>
2022-06-30 09:54:34 +0200

    adjust SWCR filters

--- src/apps/swcr/utilities/filterSetup.tsx
+++ src/apps/swcr/utilities/filterSetup.tsx
@@ -18,6 +18,7 @@ export const filterSetup: FilterOptions<SwcrPackage> = [
     {
         name: 'Project identifier',
         valueFormatter: ({ projectIdentifier }) => projectIdentifier,
+        defaultHidden: true,
     },
     {
         name: 'Contract',
@@ -42,19 +43,21 @@ export const filterSetup: FilterOptions<SwcrPackage> = [
     {
         name: 'Plant',
         valueFormatter: ({ siteCode }) => siteCode,
+        defaultHidden: true,
     },
     {
         name: 'Priority',
         valueFormatter: ({ priority }) => priority,
+        defaultHidden: true,
     },
     {
         name: 'Control system',
         valueFormatter: ({ controlSystem }) => controlSystem,
+        defaultHidden: true,
     },
     {
         name: 'Next signature by',
         valueFormatter: (swcr) => getNextToSignKeys(swcr, ''),
-        isQuickFilter: true,
     },
     {
         name: 'Next signature role',
@@ -63,33 +66,41 @@ export const filterSetup: FilterOptions<SwcrPackage> = [
     {
         name: 'Last signed ranking',
         valueFormatter: (swcr) => getLatestSignedRankingKey(swcr, ''),
+        defaultHidden: true,
     },
     {
         name: 'Action',
         valueFormatter: ({ action }) => action,
+        defaultHidden: true,
     },
     {
         name: 'Node',
         valueFormatter: ({ node }) => node,
+        defaultHidden: true,
     },
     {
         name: 'Estimated manhours',
         valueFormatter: ({ estimatedManhours }) => estimatedManhours,
+        defaultHidden: true,
     },
     {
         name: 'COMMPK no',
         valueFormatter: ({ cpkgNo }) => cpkgNo,
+        defaultHidden: true,
     },
     {
         name: 'COMM phase',
         valueFormatter: ({ cpkgPhase }) => cpkgPhase,
+        defaultHidden: true,
     },
     {
         name: 'Other references',
         valueFormatter: ({ referenceTypes }) => referenceTypes,
+        defaultHidden: true,
     },
     {
         name: 'Due date',
         valueFormatter: ({ dueAtDate }) => dueAtDate,
+        defaultHidden: true,
     },
 ];
