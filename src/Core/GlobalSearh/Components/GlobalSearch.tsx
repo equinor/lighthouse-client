24b83aefac23b9fea920e0d75b5e57f1f4b1242d
Christopher Berge Hove <chbh@equinor.com>
2022-06-30 10:48:20 +0200

    [Global Search] - Enhancement/search stability (#902)

--- src/Core/GlobalSearh/Components/GlobalSearch.tsx
+++ src/Core/GlobalSearh/Components/GlobalSearch.tsx
@@ -1,3 +1,4 @@
+import { DotProgress } from '@equinor/eds-core-react';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { ArrowNavigation } from 'react-arrow-navigation';
 import { useNavigate } from 'react-router';
@@ -6,7 +7,7 @@ import { procosysConfig } from '../Config/ProCoSys';
 import { SearchResponse } from '../Config/ProCoSys/types';
 import { useGlobalSearch } from '../Service/GlobalSearch';
 import { SearchItem } from '../Service/SearchApi';
-import { Search, SearchResult, Wrapper } from './GlobalSearchStyles';
+import { NoResult, Search, SearchResult, Wrapper } from './GlobalSearchStyles';
 import { SearchResultHeading } from './SearchResultHeading';
 import { SearchResultItem } from './SearchResultItem';
 
@@ -17,9 +18,15 @@ export const GlobalSearch = (): JSX.Element => {
     const [searchInput, setSearchInput] = useState('');
     const [placeHolderText, setPlaceholderText] = useState(SEARCH_BLUR);
     const searchRef = useRef<HTMLInputElement>(null);
+    const [isNavigationOpen, setIsNavigationOpen] = useState<boolean>(false);
+
+    function handleSetNavigation(value: boolean) {
+        setIsNavigationOpen(value);
+    }
+
     const navigate = useNavigate();
 
-    const { searchResult, search, registerSearchItem } = useGlobalSearch();
+    const { searchResult, search, isSearching, registerSearchItem } = useGlobalSearch();
 
     useEffect(() => {
         const procosysSearchRegistration = registerSearchItem<SearchResponse>(procosysConfig);
@@ -53,7 +60,10 @@ export const GlobalSearch = (): JSX.Element => {
     let keyNavigationIndex = 0;
 
     return (
-        <Wrapper>
+        <Wrapper
+            hasFocus={placeHolderText == SEARCH_FOCUS || searchInput.length > 0}
+            title={placeHolderText}
+        >
             <ArrowNavigation>
                 <Search
                     ref={searchRef}
@@ -61,39 +71,66 @@ export const GlobalSearch = (): JSX.Element => {
                     placeholder={placeHolderText}
                     onFocus={() => setPlaceholderText(SEARCH_FOCUS)}
                     onBlur={() => setPlaceholderText(SEARCH_BLUR)}
+                    isSearching={isSearching}
                     onChange={(e) => {
                         setSearchInput(e.currentTarget.value);
                     }}
                 ></Search>
-                {search.length > 0 && (
+                {isSearching && (
+                    <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
+                        <DotProgress color="primary" />
+                    </div>
+                )}
+                {searchInput.length > 0 && search.length > 0 && (
                     <SearchResult>
-                        <>
-                            {searchResult.map((searchType) => (
-                                <div key={searchType.type}>
-                                    <SearchResultHeading
-                                        typeTitle={searchType.title}
-                                        color={searchType.color}
-                                    />
-                                    {searchType.items.map((item) => {
-                                        keyNavigationIndex++;
-                                        return (
-                                            <SearchResultItem
-                                                index={keyNavigationIndex}
-                                                key={item.key}
-                                                title={item.title}
-                                                id={item.id}
-                                                description={item.description}
-                                                searchText={searchInput}
-                                                action={(id: string) => {
-                                                    searchType.action(id, item, navigate);
-                                                    setSearchInput('');
-                                                }}
-                                            />
-                                        );
-                                    })}
-                                </div>
-                            ))}
-                        </>
+                        {searchResult.length > 0 ? (
+                            <>
+                                {searchResult.map((searchType) => (
+                                    <div key={searchType.type}>
+                                        <SearchResultHeading
+                                            typeTitle={searchType.title}
+                                            color={searchType.color}
+                                        />
+                                        {searchType.items.slice(0, 5).map((item) => {
+                                            keyNavigationIndex++;
+                                            return (
+                                                <SearchResultItem
+                                                    isNavigationOpen={isNavigationOpen}
+                                                    handleNavigationOpen={handleSetNavigation}
+                                                    index={keyNavigationIndex}
+                                                    {...searchType}
+                                                    {...item}
+                                                    key={item.key}
+                                                    description={item.description}
+                                                    searchText={searchInput}
+                                                    shouldHighlightDescription={!item.group}
+                                                    appAction={
+                                                        searchType.appAction
+                                                            ? (id: string) => {
+                                                                  searchType.appAction &&
+                                                                      searchType.appAction(
+                                                                          id,
+                                                                          navigate
+                                                                      );
+                                                                  setSearchInput('');
+                                                              }
+                                                            : undefined
+                                                    }
+                                                    action={(id: string) => {
+                                                        searchType.action(id, item, navigate);
+                                                        setSearchInput('');
+                                                    }}
+                                                />
+                                            );
+                                        })}
+                                    </div>
+                                ))}
+                            </>
+                        ) : (
+                            !isSearching && (
+                                <NoResult>Sorry, Could not find what you are looking for.</NoResult>
+                            )
+                        )}
                     </SearchResult>
                 )}
             </ArrowNavigation>
--- src/Core/GlobalSearh/Config/ProCoSys/utils.ts
+++ src/Core/GlobalSearh/Config/ProCoSys/utils.ts
@@ -1,3 +1,4 @@
+import { NavigateFunction } from 'react-router';
 import { SearchItem, SearchResult } from '../../Service/SearchApi';
 import { SearchItems } from './types';
 
@@ -8,6 +9,7 @@ export function searchPushItem(
         type: string;
         title: string;
         color: string;
+        appAction?(id: string, navigate: NavigateFunction): void;
         mapper(item: SearchItems): SearchItem;
         action(id: string): void;
     }
@@ -22,6 +24,7 @@ export function searchPushItem(
             type: config.type,
             title: config.title,
             color: config.color,
+            appAction: config.appAction,
             action: config.action,
             count: 0,
             items: [config.mapper(item)],
