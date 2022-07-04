6bcdcd51bcfc8193f366a18216e2a2681d9a9f74
Joachim <joachim290@outlook.com>
2022-07-04 09:08:11 +0200

    chore: update view api for checklist

--- src/apps/Loop/utility/config/sidesheetConfig.ts
+++ src/apps/Loop/utility/config/sidesheetConfig.ts
@@ -4,9 +4,43 @@ import { setupWorkspaceSidesheet } from '../../../../Core/WorkSpace/src/WorkSpac
 
 import { LoopSidesheet } from '../../components';
 import { Loop } from '../../types';
-import { checklistColumnNames } from '../api';
 import { generateExpressions, generateFamRequest } from '../helpers/fam';
-
+const checklistColumnNames = [
+    'ChecklistId',
+    'Facility',
+    'Project',
+    'LoopId',
+    'LoopNo',
+    'Description',
+    'MechanicalCompletionPackageNo',
+    'MechanicalCompletionPackageId',
+    'CommissioningPackageNo',
+    'CommissioningPackageId',
+    'FormularType',
+    'FormularGroup',
+    'Responsible',
+    'Status',
+    'Revision',
+    'SignedDate',
+    'VerifiedDate',
+    'RFC_Planned_Forecast_Date',
+    'RFO_Planned_Forecast_Date',
+    'WOPlannedCompletionDate',
+    'WOActualCompletionDate',
+    'RemainingManHours',
+    'System',
+    'FunctionalSystem',
+    'Priority1',
+    'Priority2',
+    'Priority3',
+    'Location',
+    'IsVoided',
+    'PackageNo',
+    'CallOffNo',
+    'Register',
+    'Function',
+    'LoopContentStatus',
+];
 const idResolverFunction = async (id: string): Promise<Loop> => {
     const { FAM } = httpClient();
     const expressions = generateExpressions('checklistId', 'Equals', [id]);
