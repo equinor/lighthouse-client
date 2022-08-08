e4429497f5c2df5097614e3b5e5e61ffea11a78f
Andreas Tunheim Tønnessen <andreas.tonnessen@bouvet.no>
2022-08-08 11:19:57 +0200

    Added completedStatusName to steps

--- src/apps/ReleaseControl/components/Form/WorkflowEditor/WorkflowStep.tsx
+++ src/apps/ReleaseControl/components/Form/WorkflowEditor/WorkflowStep.tsx
@@ -5,7 +5,10 @@ import { IconMenu } from '@equinor/overlay-menu';
 import styled from 'styled-components';
 import { DRCFormAtomApi } from '../../../Atoms/formAtomApi';
 import { FunctionalRole } from '../../../types/functionalRole';
-import { CreateReleaseControlStepModel } from '../../../types/releaseControl';
+import {
+    CreateReleaseControlStepModel,
+    ReleaseControlStepNames,
+} from '../../../types/releaseControl';
 import { CriteriaRender } from '../../Workflow/Criteria';
 import { getCriteriaStatus } from '../../Workflow/Utils/getCriteriaStatus';
 import { DraggableIcon } from './DraggableIcon';
@@ -53,7 +56,7 @@ export const WorkflowStep = ({ step, steps, functionalRoles }: WorkflowStepProps
                     <NumberCircle>{step.order}</NumberCircle>
                     <Selections>
                         <SingleSelect
-                            items={stepNames}
+                            items={Object.values(ReleaseControlStepNames)}
                             label="Step"
                             size={30}
                             selectedOption={step.name}
@@ -94,7 +97,7 @@ export const WorkflowStep = ({ step, steps, functionalRoles }: WorkflowStepProps
                     <NumberCircle>{step.order}</NumberCircle>
                     <Selections>
                         <SingleSelect
-                            items={stepNames}
+                            items={Object.values(ReleaseControlStepNames)}
                             label="Step"
                             size={30}
                             selectedOption={step.name}
@@ -176,20 +179,3 @@ const HiddenDragIcon = styled.div`
     width: 24px;
     height: 24px;
 `;
-
-const stepNames = [
-    'Coordinator',
-    'Engineering',
-    'Material',
-    'Work prep',
-    'Scaffolding',
-    'Circuit isolation',
-    'Demount ISO',
-    'Check/demount HT',
-    'Demount Mech./Piping',
-    'Remount (or new) HT/A-test',
-    'Remount (or new) ISO',
-    'Recheck (or new) HT/B-test',
-    'Circuit power-up',
-    'Recheck (or new) HT/C-test',
-];
