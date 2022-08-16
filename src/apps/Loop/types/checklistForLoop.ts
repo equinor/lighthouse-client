351dd1d4a316932d8ebb3582e09addfcc65bb859
Joachim <joachim290@outlook.com>
2022-08-16 12:23:09 +0200

    chore: update endpoint for checklist loop

---
+++ src/apps/Loop/types/checklistForLoop.ts
@@ -0,0 +1,13 @@
+export type ChecklistForLoop = {
+    checklistID: string;
+    commissioningPackageId: string | null;
+    commissioningPackageNo: string | null;
+    facility: string;
+    formularGroup: string | null;
+    formularType: string | null;
+    mechanicalCompletionPackageId: string | null;
+    mechanicalCompletionPackageNo: string | null;
+    mechanicalCompletionStatus: string | null;
+    project: string | null;
+    responsible: string | null;
+};
