0d459797c94282c1e36ce36d386fe2e98e84b7d4
Joachim Senland <joachim290@gmail.com>
2022-08-18 08:37:14 +0200

    Fix/lci hanging garden uri (#966)

--- src/apps/LCI/index.ts
+++ src/apps/LCI/index.ts
@@ -2,6 +2,6 @@ import { ClientApi } from '@equinor/lighthouse-portal-client';
 
 export function setup(appApi: ClientApi): void {
     appApi.createPowerBiViewer().registerFusionPowerBi({
-        reportURI: 'pp-lci-hanging-gardens',
+        reportURI: 'pp-lci-hanging-garden',
     });
 }
