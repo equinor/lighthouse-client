cf1d7a9fc30de46eda4b1d02d44815fc2b11e97b
Joachim <joachim290@outlook.com>
2022-08-29 11:28:12 +0200

    chore: unit tests and fix status util

--- src/apps/Handover/__tests__/handoverStatuses.test.ts
+++ src/apps/Handover/__tests__/handoverStatuses.test.ts
@@ -1,14 +1,8 @@
-import {
-    getHandoverWorkOrderStatus,
-    materialOk,
-    workOrderAvailable,
-    workOrderOk,
-} from '../Garden/components/HandoverSidesheet';
 import { getStatus } from '../Garden/utility';
-import { handoverPackage, handoverWorkorder } from './mockData';
+import { handoverPackage } from './mockData';
 
-describe('Handover Package status utilities', () => {
-    test('getStatus should return RFOC Accepted when having equal counts of mcPkgsCount and mcPkgsRFOCSigned > 0', () => {
+describe('Handover Package status utility getStatus', () => {
+    it('should return RFOC Accepted when having equal counts of mcPkgsCount and mcPkgsRFOCSigned > 0', () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 1;
         testPackage.mcPkgsRFOCSigned = 1;
@@ -16,7 +10,7 @@ describe('Handover Package status utilities', () => {
         expect(status).toEqual('RFOC Accepted');
     });
 
-    test("getStatus should return RFOC Rejected when it's not RFOC Accepted and rfocIsRejected = true", () => {
+    it("should return RFOC Rejected when it's not RFOC Accepted and rfocIsRejected = true", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -26,7 +20,7 @@ describe('Handover Package status utilities', () => {
         expect(status).toEqual('RFOC Rejected');
     });
 
-    test("getStatus should return RFOC Sent when it's not RFOC Rejected and having equal counts of mcPkgsCount and mcPkgsRFOCShipped > 0", () => {
+    it("should return RFOC Sent when it's not RFOC Rejected and having equal counts of mcPkgsCount and mcPkgsRFOCShipped > 0", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 1;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -37,7 +31,7 @@ describe('Handover Package status utilities', () => {
         expect(status).toEqual('RFOC Sent');
     });
 
-    test("getStatus should return TAC Accepted when it's not RFOC Sent and tacIsAccepted = true", () => {
+    it("should return TAC Accepted when it's not RFOC Sent and tacIsAccepted = true", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -48,7 +42,7 @@ describe('Handover Package status utilities', () => {
         const status = getStatus(testPackage);
         expect(status).toEqual('TAC Accepted');
     });
-    test("getStatus should return TAC Sent when it's not TAC Accepted and tacIsShipped = true", () => {
+    it("should return TAC Sent when it's not TAC Accepted and tacIsShipped = true", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -61,7 +55,7 @@ describe('Handover Package status utilities', () => {
         expect(status).toEqual('TAC Sent');
     });
 
-    test("getStatus should return TAC Rejected when it's not TAC Sent and tacIsRejected = true", () => {
+    it("should return TAC Rejected when it's not TAC Sent and tacIsRejected = true", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -74,7 +68,7 @@ describe('Handover Package status utilities', () => {
         const status = getStatus(testPackage);
         expect(status).toEqual('TAC Rejected');
     });
-    test("getStatus should return RFCC Accepted when it's not TAC Rejected and having equal counts for mcPkgsCount and mcPkgsRFCCSigned > 0 ", () => {
+    it("should return RFCC Accepted when it's not TAC Rejected and having equal counts for mcPkgsCount and mcPkgsRFCCSigned > 0 ", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 1;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -88,7 +82,7 @@ describe('Handover Package status utilities', () => {
         const status = getStatus(testPackage);
         expect(status).toEqual('RFCC Accepted');
     });
-    test("getStatus should return RFCC Rejected when it's not RFCC Accepted and rfccRejected = true ", () => {
+    it("should return RFCC Rejected when it's not RFCC Accepted and rfccRejected = true ", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -103,7 +97,7 @@ describe('Handover Package status utilities', () => {
         const status = getStatus(testPackage);
         expect(status).toEqual('RFCC Rejected');
     });
-    test("getStatus should return RFCC Sent when it's not RFCC Rejected and having equal counts of mcPkgsCount and mcPkgsRFCCShippedCount > 0 ", () => {
+    it("should return RFCC Sent when it's not RFCC Rejected and having equal counts of mcPkgsCount and mcPkgsRFCCShippedCount > 0 ", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 1;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -119,7 +113,7 @@ describe('Handover Package status utilities', () => {
         const status = getStatus(testPackage);
         expect(status).toEqual('RFCC Sent');
     });
-    test("getStatus should return RFRC Accepted when it's not RFCC Sent and isDemolition = true and demolitionActualFinishDate exists ", () => {
+    it("should return RFRC Accepted when it's not RFCC Sent and isDemolition = true and demolitionActualFinishDate exists ", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -137,7 +131,7 @@ describe('Handover Package status utilities', () => {
         const status = getStatus(testPackage);
         expect(status).toEqual('RFRC Accepted');
     });
-    test("getStatus should return RFRC Sent when it's not RFRC Accepted and isDemolition = true and demolitionRFRCShippedDate exists ", () => {
+    it("should return RFRC Sent when it's not RFRC Accepted and isDemolition = true and demolitionRFRCShippedDate exists ", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -157,7 +151,7 @@ describe('Handover Package status utilities', () => {
         expect(status).toEqual('RFRC Sent');
     });
 
-    test("getStatus should return DCC Accepted when it's not RFRC Sent and isDemolition = true and demolitionDCCAcceptedDate exists ", () => {
+    it("should return DCC Accepted when it's not RFRC Sent and isDemolition = true and demolitionDCCAcceptedDate exists ", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -178,7 +172,7 @@ describe('Handover Package status utilities', () => {
         expect(status).toEqual('DCC Accepted');
     });
 
-    test("getStatus should return DCC Sent when it's not DCC Accepted and isDemolition = true and demolitionActualStartDate exists ", () => {
+    it("should return DCC Sent when it's not DCC Accepted and isDemolition = true and demolitionActualStartDate exists ", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -200,7 +194,7 @@ describe('Handover Package status utilities', () => {
         expect(status).toEqual('DCC Sent');
     });
 
-    test("getStatus should return OS when it's not DCC Sent ", () => {
+    it("should return OS when it's not DCC Sent ", () => {
         let testPackage = handoverPackage;
         testPackage.mcPkgsCount = 0;
         testPackage.mcPkgsRFOCSigned = 0;
@@ -220,60 +214,5 @@ describe('Handover Package status utilities', () => {
 
         const status = getStatus(testPackage);
         expect(status).toEqual('OS');
-    });
-});
-
-describe('Handover sidesheet status utilities', () => {
-    test('materialOk should return length 1 when Handover WO has materialStatus = MN', () => {
-        let testPackage = handoverWorkorder;
-        testPackage.materialStatus = 'MN';
-        const mnLength = materialOk(testPackage);
-        expect(mnLength).toEqual(1);
-    });
-    test('materialOk should return length 2 when Handover WO has materialStatus = M12 | M13', () => {
-        let testPackage = handoverWorkorder;
-        testPackage.materialStatus = 'M12';
-        const m12Length = materialOk(testPackage);
-        expect(m12Length).toEqual(2);
-        testPackage.materialStatus = 'M13';
-        const m13Length = materialOk(testPackage);
-        expect(m13Length).toEqual(2);
-    });
-    test('materialOk should return length 0 when Handover WO does not have materialStatus = MN | M12 | M13', () => {
-        let testPackage = handoverWorkorder;
-        testPackage.materialStatus = 'MX';
-        const mxLength = materialOk(testPackage);
-        expect(mxLength).toEqual(0);
-    });
-
-    test('workOrderOk should return true when workOrderStatus equals W04..W10', () => {
-        let testPackage = handoverWorkorder;
-        testPackage.workOrderStatus = 'W04';
-        const workOrderOkBool = workOrderOk(testPackage);
-        expect(workOrderOkBool).toEqual(true);
-    });
-
-    test('workOrderAvailable should return true when workOrderStatus equals W03..W10', () => {
-        let testPackage = handoverWorkorder;
-        testPackage.workOrderStatus = 'W04';
-        const workOrderOkBool = workOrderAvailable(testPackage);
-        expect(workOrderOkBool).toEqual(true);
-    });
-
-    test('getHandoverWorkorderStatus should return WO Finished when projectProgress = 100', () => {
-        let testPackage = handoverWorkorder;
-        testPackage.projectProgress = '100';
-        const status = getHandoverWorkOrderStatus(testPackage);
-
-        expect(status).toEqual('WO Finished');
-    });
-    test("getHandoverWorkorderStatus should Material and Workorder OK when it's not WO Finished and WO status = W04..W10 and WO mat status = m12 | m13 | mn ", () => {
-        let testPackage = handoverWorkorder;
-        testPackage.projectProgress = '0';
-        testPackage.workOrderStatus = 'M12';
-        testPackage.materialStatus = 'mn';
-        const status = getHandoverWorkOrderStatus(testPackage);
-
-        expect(status).toEqual('Material and Workorder OK');
     });
 });
---
+++ src/apps/Handover/__tests__/sidesheetStatuses.test.ts
@@ -0,0 +1,85 @@
+import {
+    getHandoverWorkOrderStatus,
+    materialOk,
+    workOrderAvailable,
+    workOrderOk,
+} from '../Garden/components/HandoverSidesheet';
+import { handoverWorkorder } from './mockData';
+
+describe('materialOk function', () => {
+    it('should return length 1 when Handover WO has materialStatus = MN', () => {
+        let testPackage = handoverWorkorder;
+        testPackage.materialStatus = 'MN';
+        const mnLength = materialOk(testPackage);
+        expect(mnLength).toEqual(1);
+    });
+    it('should return length 2 when Handover WO has materialStatus = M12 | M13', () => {
+        let testPackage = handoverWorkorder;
+        testPackage.materialStatus = 'M12';
+        const m12Length = materialOk(testPackage);
+        expect(m12Length).toEqual(2);
+        testPackage.materialStatus = 'M13';
+        const m13Length = materialOk(testPackage);
+        expect(m13Length).toEqual(2);
+    });
+    it('should  return length 0 when Handover WO does not have materialStatus = MN | M12 | M13', () => {
+        let testPackage = handoverWorkorder;
+        testPackage.materialStatus = 'MX';
+        const mxLength = materialOk(testPackage);
+        expect(mxLength).toEqual(0);
+    });
+});
+
+describe('Workorder utility functions', () => {
+    test('workorderOK should should return true when workOrderStatus equals W04..W10', () => {
+        let testPackage = handoverWorkorder;
+        testPackage.workOrderStatus = 'W04';
+        const workOrderOkBool = workOrderOk(testPackage);
+        expect(workOrderOkBool).toEqual(true);
+    });
+
+    test('workOrderAvailable should return true when workOrderStatus equals W03..W10', () => {
+        let testPackage = handoverWorkorder;
+        testPackage.workOrderStatus = 'W04';
+        const workOrderOkBool = workOrderAvailable(testPackage);
+        expect(workOrderOkBool).toEqual(true);
+    });
+});
+
+describe('getHandoverWorkorderStatus ', () => {
+    it('should return WO Finished when projectProgress = 100', () => {
+        let testPackage = handoverWorkorder;
+        testPackage.projectProgress = '100';
+        const status = getHandoverWorkOrderStatus(testPackage);
+
+        expect(status).toEqual('WO Finished');
+    });
+    it("should return Material and Workorder OK when it's not WO Finished and WO status = W04..W10 and WO mat status = m12 | m13 | mn ", () => {
+        let testPackage = handoverWorkorder;
+        testPackage.projectProgress = '0';
+        testPackage.workOrderStatus = 'W05';
+        testPackage.materialStatus = 'MN';
+        const status = getHandoverWorkOrderStatus(testPackage);
+
+        expect(status).toEqual('Material and Workorder OK');
+    });
+
+    it("should return Material and Workorder Available when it's not Material and Workorder OK and materialStatus = m7, m8, m10, m11, mn and woStatus = W03..W10 ", () => {
+        let testPackage = handoverWorkorder;
+        testPackage.projectProgress = '0';
+        testPackage.workOrderStatus = 'W05';
+        testPackage.materialStatus = 'M11';
+        const status = getHandoverWorkOrderStatus(testPackage);
+        expect(status).toEqual('Material and Workorder Available');
+    });
+
+    it("should return Material and/or Workorder not Available when it's not any other status", () => {
+        let testPackage = handoverWorkorder;
+        testPackage.projectProgress = '0';
+        testPackage.workOrderStatus = 'W04';
+        testPackage.materialStatus = 'MX';
+
+        const status = getHandoverWorkOrderStatus(testPackage);
+        expect(status).toEqual('Material and/or Workorder not Available');
+    });
+});
