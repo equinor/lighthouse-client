d29b992b7cf59dba78aeb2ac6896b350f98270ce
Joachim <joachim290@outlook.com>
2022-09-05 08:39:05 +0200

    feat: component for excel export in table

--- src/packages/Table/index.ts
+++ src/packages/Table/index.ts
@@ -1,5 +1,7 @@
 export * from './Components/Table';
 export * from './Components/Cells';
+export * from './Components/ExportToExcel';
+export * from './Components/ColumnPicker/ColumnPicker';
 export * from './Hooks';
 export * from './Types';
 export { defaultGroupByFn } from './Utils/utils';
