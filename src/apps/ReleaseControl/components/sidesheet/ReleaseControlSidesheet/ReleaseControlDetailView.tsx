051e2163faf0a419cab5972dcd1da8cb4fe10c30
Andreas Tunheim Tønnessen <andreas.tonnessen@bouvet.no>
2022-09-13 15:00:57 +0200

    Scope change references for RC + sidesheet fix

---
+++ src/apps/ReleaseControl/components/sidesheet/ReleaseControlSidesheet/ReleaseControlDetailView.tsx
@@ -0,0 +1,34 @@
+import { Tabs } from '@equinor/eds-core-react';
+import { useEdsTabs } from '@equinor/hooks';
+import { ReleaseControlSidesheetBanner } from './ReleaseControlSidesheetBanner';
+import { HeaderTab, SidesheetTabList, Tab, TabList } from './sidesheetStyles';
+import { HistoryTab } from './Tabs/HistoryTab';
+import { ScopeTab } from './Tabs/ScopeTab';
+import { WorkflowTab } from './Tabs/WorkflowTab';
+
+export const ReleaseControlDetailView = (): JSX.Element => {
+    const { activeTab, handleChange } = useEdsTabs();
+    return (
+        <>
+            <ReleaseControlSidesheetBanner></ReleaseControlSidesheetBanner>
+            <Tabs activeTab={activeTab} onChange={handleChange}>
+                <SidesheetTabList>
+                    <HeaderTab>Scope</HeaderTab>
+                    <HeaderTab>Workflow</HeaderTab>
+                    <HeaderTab>History</HeaderTab>
+                </SidesheetTabList>
+                <TabList>
+                    <Tab>
+                        <ScopeTab />
+                    </Tab>
+                    <Tab>
+                        <WorkflowTab />
+                    </Tab>
+                    <Tab>
+                        <HistoryTab />
+                    </Tab>
+                </TabList>
+            </Tabs>
+        </>
+    );
+};
