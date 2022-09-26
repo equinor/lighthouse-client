f262db59e74cd156dbbc7468b6a6941460b7df14
Andreas Tunheim Tønnessen <andreas.tonnessen@bouvet.no>
2022-09-26 13:12:49 +0200

    Improved status bar KPIs for piping&ht

--- src/apps/DisciplineReleaseControl/utils/config/StatusBar/statusBarConfig.ts
+++ src/apps/DisciplineReleaseControl/utils/config/StatusBar/statusBarConfig.ts
@@ -1,24 +1,86 @@
 import { StatusItem } from '@equinor/lighthouse-status-bar';
-import { PipetestStep } from '../../../Types/drcEnums';
+import { PipetestCheckListOrder, PipetestStep } from '../../../Types/drcEnums';
 import { Pipetest } from '../../../Types/pipetest';
+import { getPipetestStatusSortValue } from '../../helpers/statusHelpers';
 import { getPipetestsWithHTCable } from '../../helpers/tableHelpers';
 
 export function statusBarConfig(data: Pipetest[]): StatusItem[] {
+    const pipetestsWithHTCable = getPipetestsWithHTCable(data);
     return [
         {
-            title: 'Heat trace installed',
+            title: 'Pipetest total',
             value: () => {
-                return getPipetestsWithHTCable(data)
-                    .filter((x) => x.step === PipetestStep.Complete)
-                    .length.toString();
+                return data?.length?.toString();
             },
         },
         {
-            title: 'Heat trace planned',
+            title: 'Piping open',
             value: () => {
-                return getPipetestsWithHTCable(data)
-                    .filter((x) => x.step !== PipetestStep.Complete)
-                    .length.toString();
+                return data
+                    ?.filter(
+                        (x) =>
+                            getPipetestStatusSortValue(x) <= PipetestCheckListOrder.Bolttensioning
+                    )
+                    ?.length?.toString();
+            },
+        },
+        {
+            title: 'Piping % complete',
+            value: () => {
+                return (
+                    (
+                        (100 *
+                            data?.filter(
+                                (x) =>
+                                    getPipetestStatusSortValue(x) >
+                                    PipetestCheckListOrder.Bolttensioning
+                            )?.length) /
+                        data?.length
+                    )
+                        ?.toFixed(2)
+                        ?.toString() + '%'
+                );
+            },
+        },
+        {
+            title: 'HT total',
+            value: () => {
+                return pipetestsWithHTCable?.length?.toString();
+            },
+        },
+        {
+            title: 'HT open A-test',
+            value: () => {
+                return pipetestsWithHTCable
+                    .filter((x) => getPipetestStatusSortValue(x) <= PipetestCheckListOrder.HtTest)
+                    ?.length?.toString();
+            },
+        },
+        {
+            title: 'HT open B-test',
+            value: () => {
+                return pipetestsWithHTCable
+                    .filter((x) => getPipetestStatusSortValue(x) <= PipetestCheckListOrder.HtRetest)
+                    ?.length?.toString();
+            },
+        },
+
+        {
+            title: 'HT % complete',
+            value: () => {
+                return (
+                    (
+                        (100 *
+                            pipetestsWithHTCable.filter(
+                                (x) =>
+                                    x.step === PipetestStep.Marking ||
+                                    x.step === PipetestStep.Complete
+                            )?.length) /
+                        pipetestsWithHTCable?.length
+                    )
+                        ?.toFixed(2)
+                        ?.toString() + '%'
+                );
             },
         },
     ];
