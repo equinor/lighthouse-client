f6f1b8317a5a85b422238a62175fa4c7183725dd
Andreas Tunheim Tønnessen <andreas.tonnessen@bouvet.no>
2022-09-26 13:49:00 +0200

    Better number formatting

--- src/apps/DisciplineReleaseControl/utils/config/StatusBar/statusBarConfig.ts
+++ src/apps/DisciplineReleaseControl/utils/config/StatusBar/statusBarConfig.ts
@@ -4,64 +4,72 @@ import { Pipetest } from '../../../Types/pipetest';
 import { getPipetestStatusSortValue } from '../../helpers/statusHelpers';
 import { getPipetestsWithHTCable } from '../../helpers/tableHelpers';
 
+function numberFormat(number: number): string {
+    return parseFloat(Math.round(number).toString()).toLocaleString('no');
+}
+
 export function statusBarConfig(data: Pipetest[]): StatusItem[] {
     const pipetestsWithHTCable = getPipetestsWithHTCable(data);
     return [
         {
             title: 'Pipetest total',
             value: () => {
-                return data?.length?.toString();
+                return numberFormat(data?.length);
             },
         },
         {
             title: 'Piping open',
             value: () => {
-                return data
-                    ?.filter(
+                return numberFormat(
+                    data?.filter(
                         (x) =>
                             getPipetestStatusSortValue(x) <= PipetestCheckListOrder.Bolttensioning
-                    )
-                    ?.length?.toString();
+                    )?.length
+                );
             },
         },
         {
             title: 'Piping % complete',
             value: () => {
                 return (
-                    (
-                        (100 *
-                            data?.filter(
-                                (x) =>
-                                    getPipetestStatusSortValue(x) >
-                                    PipetestCheckListOrder.Bolttensioning
-                            )?.length) /
-                        data?.length
-                    )
-                        ?.toFixed(2)
-                        ?.toString() + '%'
+                    Number(
+                        (
+                            (100 *
+                                data?.filter(
+                                    (x) =>
+                                        getPipetestStatusSortValue(x) >
+                                        PipetestCheckListOrder.Bolttensioning
+                                )?.length) /
+                            data?.length
+                        ).toFixed(2)
+                    ) + '%'
                 );
             },
         },
         {
             title: 'HT total',
             value: () => {
-                return pipetestsWithHTCable?.length?.toString();
+                return numberFormat(pipetestsWithHTCable?.length);
             },
         },
         {
             title: 'HT open A-test',
             value: () => {
-                return pipetestsWithHTCable
-                    .filter((x) => getPipetestStatusSortValue(x) <= PipetestCheckListOrder.HtTest)
-                    ?.length?.toString();
+                return numberFormat(
+                    pipetestsWithHTCable.filter(
+                        (x) => getPipetestStatusSortValue(x) <= PipetestCheckListOrder.HtTest
+                    )?.length
+                );
             },
         },
         {
             title: 'HT open B-test',
             value: () => {
-                return pipetestsWithHTCable
-                    .filter((x) => getPipetestStatusSortValue(x) <= PipetestCheckListOrder.HtRetest)
-                    ?.length?.toString();
+                return numberFormat(
+                    pipetestsWithHTCable.filter(
+                        (x) => getPipetestStatusSortValue(x) <= PipetestCheckListOrder.HtRetest
+                    )?.length
+                );
             },
         },
 
@@ -69,17 +77,17 @@ export function statusBarConfig(data: Pipetest[]): StatusItem[] {
             title: 'HT % complete',
             value: () => {
                 return (
-                    (
-                        (100 *
-                            pipetestsWithHTCable.filter(
-                                (x) =>
-                                    x.step === PipetestStep.Marking ||
-                                    x.step === PipetestStep.Complete
-                            )?.length) /
-                        pipetestsWithHTCable?.length
-                    )
-                        ?.toFixed(2)
-                        ?.toString() + '%'
+                    Number(
+                        (
+                            (100 *
+                                pipetestsWithHTCable.filter(
+                                    (x) =>
+                                        x.step === PipetestStep.Marking ||
+                                        x.step === PipetestStep.Complete
+                                )?.length) /
+                            pipetestsWithHTCable?.length
+                        ).toFixed(2)
+                    ) + '%'
                 );
             },
         },
