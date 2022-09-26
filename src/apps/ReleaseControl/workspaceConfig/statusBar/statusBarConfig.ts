edc973fa54c7528985cdfc86c01cc6e099ce583b
Andreas Tunheim Tønnessen <andreas.tonnessen@bouvet.no>
2022-09-26 13:53:42 +0200

    Better number formatting

--- src/apps/ReleaseControl/workspaceConfig/statusBar/statusBarConfig.ts
+++ src/apps/ReleaseControl/workspaceConfig/statusBar/statusBarConfig.ts
@@ -2,35 +2,39 @@ import { StatusItem } from '@equinor/lighthouse-status-bar';
 import moment from 'moment';
 import { ReleaseControl } from '../../types/releaseControl';
 
+function numberFormat(number: number): string {
+    return parseFloat(Math.round(number).toString()).toLocaleString('no');
+}
+
 export function statusBarConfig(data: ReleaseControl[]): StatusItem[] {
     return [
         {
             title: 'Total',
             value: () => {
-                return data?.length?.toString();
+                return numberFormat(data?.length);
             },
         },
         {
             title: 'Open',
             value: () => {
-                return data?.filter((x) => !x.isVoided && x.state === 'Open')?.length?.toString();
+                return numberFormat(data?.filter((x) => !x.isVoided && x.state === 'Open')?.length);
             },
         },
         {
             title: 'Overdue',
             value: () => {
-                return data
-                    ?.filter((x) => moment(x.plannedDueDate).isBefore(Date.now()))
-                    ?.length?.toString();
+                return numberFormat(
+                    data?.filter((x) => moment(x.plannedDueDate).isBefore(Date.now()))?.length
+                );
             },
         },
         {
             title: '% closed',
             value: () => {
                 return (
-                    ((100 * data?.filter((x) => x.state === 'Closed').length) / data?.length)
-                        ?.toFixed(2)
-                        ?.toString() + '%'
+                    Number(
+                        (100 * data?.filter((x) => x.state === 'Closed').length) / data?.length
+                    )?.toFixed(2) + '%'
                 );
             },
         },
