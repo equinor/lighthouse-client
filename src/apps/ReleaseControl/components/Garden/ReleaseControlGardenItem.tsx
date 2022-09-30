e347e66199ade19c5cb909b8f51626dc8d2d2361
Andreas Tunheim Tønnessen <andreas.tonnessen@bouvet.no>
2022-09-30 14:24:24 +0200

    Split description from item in RC and P&HT gardens

---
+++ src/apps/ReleaseControl/components/Garden/ReleaseControlGardenItem.tsx
@@ -0,0 +1,30 @@
+import { ReleaseControlItem, MidSection, Root, Title } from './GardenItemStyles';
+import { CustomItemView } from '@equinor/ParkView';
+
+import { memo } from 'react';
+import { ReleaseControl } from '../../types/releaseControl';
+
+const ReleaseControlGardenItem = ({
+    data,
+    itemKey,
+    onClick,
+    columnExpanded,
+    isSelected,
+}: CustomItemView<ReleaseControl>) => {
+    return (
+        <Root>
+            <ReleaseControlItem
+                isSelected={isSelected}
+                onClick={onClick}
+                isExpanded={columnExpanded}
+            >
+                <MidSection expanded={columnExpanded}>
+                    <Title>{data[itemKey]}</Title>
+                </MidSection>
+            </ReleaseControlItem>
+            {columnExpanded && data.description}
+        </Root>
+    );
+};
+
+export default memo(ReleaseControlGardenItem);
