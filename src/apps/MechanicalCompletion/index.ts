11be9a6964b13a62e7e89670f1ab8a7abd358303
Joachim Senland <joachim290@gmail.com>
2022-10-24 14:26:20 +0200

    [Apps]- fix: add description to search options (#1080)

--- src/apps/MechanicalCompletion/index.ts
+++ src/apps/MechanicalCompletion/index.ts
@@ -56,6 +56,7 @@ export function setup(addApi: ClientApi): void {
         .registerSearchOptions([
             { name: 'Mc pkg', valueFormatter: ({ mcPkgNumber }) => mcPkgNumber },
             { name: 'Comm pkg', valueFormatter: (pkg) => pkg.commPkgNumber },
+            { name: 'Description', valueFormatter: (pkg) => pkg.description },
         ])
 
         .registerTableOptions(tableConfig)
