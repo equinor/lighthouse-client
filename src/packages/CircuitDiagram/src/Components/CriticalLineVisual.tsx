6483bdb6b0722829b5d76bd1be7c2077db46f8de
Andreas-Tonnessen <47244521+Andreas-Tonnessen@users.noreply.github.com>
2022-11-30 12:06:45 +0100

    Hover over tooltip for critical line in circuit diagram (#1151)

--- src/packages/CircuitDiagram/src/Components/CriticalLineVisual.tsx
+++ src/packages/CircuitDiagram/src/Components/CriticalLineVisual.tsx
@@ -1,11 +1,29 @@
-import { CriticalLineVisualStyle, TestDotCircleText, TestDotWrapper } from '../../styles/styles';
+import { useState } from 'react';
+import {
+    CircuitDiagramPopover,
+    CriticalLineVisualStyle,
+    TestDotCircleText,
+    TestDotWrapper,
+} from '../../styles/styles';
 
 export const CriticalLineVisual = (): JSX.Element => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const onOpen = () => setIsOpen(true);
+    const onClose = () => setIsOpen(false);
+
     return (
-        <TestDotWrapper>
-            <CriticalLineVisualStyle>
-                <TestDotCircleText>CL</TestDotCircleText>
-            </CriticalLineVisualStyle>
-        </TestDotWrapper>
+        <>
+            <TestDotWrapper onMouseOver={onOpen} onMouseLeave={onClose}>
+                <CriticalLineVisualStyle>
+                    <TestDotCircleText>CL</TestDotCircleText>
+                    {isOpen && (
+                        <div>
+                            <CircuitDiagramPopover>{'Heating Critical Line'}</CircuitDiagramPopover>
+                        </div>
+                    )}
+                </CriticalLineVisualStyle>
+            </TestDotWrapper>
+        </>
     );
 };
