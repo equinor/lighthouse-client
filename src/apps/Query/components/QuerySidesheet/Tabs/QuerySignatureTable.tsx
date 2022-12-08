abe72df14f07a9a86d1813beb8702a1931b7f100
Jone Ueland <73601851+JoneUE@users.noreply.github.com>
2022-12-08 11:06:11 +0100

    [Query] - Feat Query workspace (#1142)

---
+++ src/apps/Query/components/QuerySidesheet/Tabs/QuerySignatureTable.tsx
@@ -0,0 +1,71 @@
+import { generateExpressions, generateFamRequest } from '@equinor/fam-request-builder';
+import { TabTable } from '@equinor/GardenUtils';
+import { useGetUser } from '@equinor/hooks';
+import { CellProps, Column } from '@equinor/Table';
+import { useQuery } from 'react-query';
+import { Query, QuerySignature } from '../../../types';
+import { getQuerySignatures, querySignaturesColumnNames } from '../../../utility/api';
+import { sortBySequence } from '../../../utility/helpers';
+const columns: Column<QuerySignature>[] = [
+    {
+        id: 'SignatureRole',
+        Header: 'Signature role',
+        accessor: (pkg) => pkg.signatureRole,
+        width: 150,
+    },
+    {
+        id: 'Sequence',
+        Header: 'Sequence',
+        accessor: (pkg) => pkg.sequence,
+        width: 100,
+        sortType: 'number',
+    },
+    {
+        Header: 'By',
+        accessor: (pkg) => pkg.signedByAzureOid,
+        Cell: (cellProps: CellProps<QuerySignature, string | null>) => {
+            const { error, isLoadingUser, user } = useGetUser(cellProps.value ?? undefined);
+
+            if (isLoadingUser) {
+                return <div>....</div>;
+            }
+            if (error || !user) {
+                return <div>{cellProps.row.original.functionalRole}</div>;
+            }
+            return <div>{user.name}</div>;
+        },
+        width: 150,
+    },
+    {
+        id: 'signedDate',
+        Header: 'Signed date',
+        accessor: (pkg) => pkg.signedDate,
+        Aggregated: () => null,
+        aggregate: 'count',
+        width: 100,
+        Cell: (cellProps) => {
+            return cellProps.value ? new Date(cellProps.value).toLocaleDateString() : '';
+        },
+    },
+];
+type QuerySignatureTableProps = {
+    query: Query;
+};
+export const QuerySignatureTable = ({ query }: QuerySignatureTableProps): JSX.Element => {
+    const expressions = generateExpressions('queryNo', 'Equals', [query.queryNo]);
+    const requestArgs = generateFamRequest(querySignaturesColumnNames, 'Or', expressions);
+    const { data, isLoading, error } = useQuery(['querySignature', query.queryNo], ({ signal }) =>
+        getQuerySignatures(requestArgs, signal)
+    );
+
+    return (
+        <TabTable
+            columns={columns}
+            packages={data ? data.sort(sortBySequence) : data}
+            error={error instanceof Error ? error : null}
+            isFetching={isLoading}
+            resourceName="Query signature"
+            height={300}
+        />
+    );
+};
---
+++ src/apps/Query/utility/api/index.ts
@@ -0,0 +1,2 @@
+export * from './getQueryCommpkgs';
+export * from './getQuerySignatureData';
