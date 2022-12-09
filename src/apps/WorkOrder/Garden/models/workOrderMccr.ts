13ae6c5db10d10c5baea4aaed46b97146e1b8523
Joachim <joachim290@outlook.com>
2022-12-09 13:36:19 +0100

    chore: url fields for procosys/workorder/mccr

--- src/apps/WorkOrder/Garden/models/workOrderMccr.ts
+++ src/apps/WorkOrder/Garden/models/workOrderMccr.ts
@@ -2,13 +2,17 @@ export type WorkOrderMccr = {
     workOrderId: string | null;
     tagNumber: string | null;
     tagId: string | null;
+    tagUrlId: string | null;
     description: string | null;
     mccrType: string | null;
     mccrStatus: string | null;
     mccrResponsible: string | null;
     mccrId: string | null;
+    mccrUrlId: string | null;
     mcpkgNumber: string | null;
     mcPkgId: string | null;
+    mechanicalCompletionPackageUrlId: string | null;
     commpkgId: string | null;
+    commissioningPackageUrlId: string | null;
     commpkgNumber: string | null;
 };
