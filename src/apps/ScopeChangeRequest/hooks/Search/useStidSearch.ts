0ea508eb7da1ea744723acac28abd026f5add210
Joachim Senland <joachim290@gmail.com>
2022-12-13 10:48:14 +0100

    Revert "Document batch search (#1172)"
    This reverts commit ff9a41ec8987d23a7ba611ac526a10556a66cd34.

--- src/apps/ScopeChangeRequest/hooks/Search/useStidSearch.ts
+++ src/apps/ScopeChangeRequest/hooks/Search/useStidSearch.ts
@@ -2,7 +2,6 @@ import { useFacility } from '@equinor/lighthouse-portal-client';
 import { TypedSelectOption } from '../../api/Search/searchType';
 import { searchDocuments } from '../../api/Search/STID/searchDocuments';
 import { searchTags } from '../../api/Search/STID/searchTags';
-import { Document } from '../../types/STID/document';
 import { StidTypes } from '../../types/STID/STIDTypes';
 
 interface StidSearch {
@@ -29,29 +28,8 @@ export function useSTIDSearch(): StidSearch {
             case 'stidtag': {
                 return await searchTags(searchValue, facilityId, signal);
             }
-
             case 'document': {
-                const documents = await searchDocuments(searchValue, facilityId, signal);
-                //We have to filter out only the OF-P documents if there are two documents with same docNo
-                const uniqueDocs = documents.reduce((unique: TypedSelectOption[], o) => {
-                    {
-                        if (
-                            documents.some(
-                                (x) =>
-                                    (x.object as Document).docNo === (o.object as Document).docNo &&
-                                    (o.object as Document).revStatus === 'OF-P'
-                            ) ||
-                            documents.filter(
-                                (x) => (x.object as Document).docNo === (o.object as Document).docNo
-                            ).length <= 1
-                        ) {
-                            unique.push(o);
-                        }
-                    }
-                    return unique;
-                }, []);
-
-                return uniqueDocs;
+                return await searchDocuments(searchValue, facilityId, signal);
             }
 
             default: {
