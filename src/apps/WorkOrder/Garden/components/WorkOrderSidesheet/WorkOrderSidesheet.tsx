01c084d8e2125b212877d645b0372c16554de03a
Joachim <joachim290@outlook.com>
2022-12-14 09:30:27 +0100

    chore: use guid for endpoints

--- src/apps/WorkOrder/Garden/components/WorkOrderSidesheet/WorkOrderSidesheet.tsx
+++ src/apps/WorkOrder/Garden/components/WorkOrderSidesheet/WorkOrderSidesheet.tsx
@@ -36,8 +36,8 @@ export const WorkorderSideSheet = ({ item, actions }: WorkorderSideSheetProps): 
         material,
         isFetching: materialIsFetching,
         error: materialError,
-    } = useMaterial(item.workOrderUrlId);
-    const { mccr, isFetching: mccrIsFetching, error: mccrError } = useMccr(item.workOrderUrlId);
+    } = useMaterial(item.workOrderId);
+    const { mccr, isFetching: mccrIsFetching, error: mccrError } = useMccr(item.workOrderId);
 
     return (
         <SideSheetContainer>
