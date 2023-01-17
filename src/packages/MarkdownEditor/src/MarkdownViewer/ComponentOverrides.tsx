03f6e230b2bba577919c359c5069659dea49f2a9
Joachim <joachim290@outlook.com>
2023-01-17 10:41:32 +0100

    fix: font sizes for md viewer

--- src/packages/MarkdownEditor/src/MarkdownViewer/ComponentOverrides.tsx
+++ src/packages/MarkdownEditor/src/MarkdownViewer/ComponentOverrides.tsx
@@ -1,5 +1,5 @@
 export const LI = ({ children }: { children: JSX.Element }): JSX.Element => {
-    return <li style={{ fontSize: '14px' }}>{children}</li>;
+    return <li style={{ fontSize: '16px' }}>{children}</li>;
 };
 export const OL = ({ children }: { children: JSX.Element }): JSX.Element => {
     return <ol style={{ lineHeight: '1em' }}>{children}</ol>;
