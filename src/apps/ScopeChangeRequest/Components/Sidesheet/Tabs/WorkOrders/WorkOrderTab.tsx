a1c27a4ff8f528ab1717ba39ca827da4512980a3
Joachim <joachim290@gmail.com>
2023-03-07 14:14:33 +0100

    [SCR] - feat: add not found wos to wo table (#1289)
    Co-authored-by: Joachim <joachim290@outlook.com>

--- src/apps/ScopeChangeRequest/Components/Sidesheet/Tabs/WorkOrders/WorkOrderTab.tsx
+++ src/apps/ScopeChangeRequest/Components/Sidesheet/Tabs/WorkOrders/WorkOrderTab.tsx
@@ -5,7 +5,7 @@ import { useQuery } from 'react-query';
 import { CompactWorkOrderList } from '../../../WorkOrderTable/CompactWorkOrder/CompactWorkOrdersList';
 import { Loading, NoWorkOrders, Wrapper } from './workOrderTab.styles';
 import { useScopeChangeContext } from '../../../../hooks/context/useScopeChangeContext';
-import { getWorkOrderByIds } from '@equinor/Workflow';
+import { getWorkOrderByIds, WorkOrder } from '@equinor/Workflow';
 
 export function WorkOrderTab(): JSX.Element {
     const woNumbers =
@@ -47,16 +47,68 @@ export function WorkOrderTab(): JSX.Element {
         );
     }
 
+    const mergedWos = mergeWos(data, woNumbers);
+
     return (
         <Wrapper>
             {data?.length !== woNumbers.length && <i>* Some work orders did not load correctly</i>}
             <div ref={ref}>
                 {width > 960 ? (
-                    <WorkOrderTable workOrders={data ?? []} />
+                    <WorkOrderTable workOrders={mergedWos ?? []} />
                 ) : (
-                    <CompactWorkOrderList workOrders={data ?? []} />
+                    <CompactWorkOrderList workOrders={mergedWos ?? []} />
                 )}
             </div>
         </Wrapper>
     );
+}
+/** Find the WO numbers that did not get retrieved correctly from FAM/PCS */
+function findNotFoundWoNos(famWos: WorkOrder[], woNos: string[]) {
+    const notFoundWoNos: string[] = [];
+    woNos.forEach((woNo) => {
+        if (famWos.findIndex((workOrder) => workOrder.workOrderNumber === woNo) === -1) {
+            notFoundWoNos.push(woNo);
+        }
+    });
+    return notFoundWoNos;
+}
+
+/** Merge potentially not found WOs with the WOs retrieved from FAM */
+function mergeWos(famWos: WorkOrder[] = [], woNos: string[]) {
+    /** If all WOs are retrieved correctly, just return early */
+    if (famWos.length === woNos.length) {
+        return famWos;
+    }
+
+    const notFoundWoNos = findNotFoundWoNos(famWos, woNos);
+
+    const notFoundWos: WorkOrder[] = notFoundWoNos.map((woNo) => ({
+        workOrderNumber: woNo,
+        actualCompletionDate: null,
+        actualStartupDate: null,
+        description: 'WORK ORDER NOT FOUND',
+        discipline: null,
+        disciplineCode: null,
+        estimatedHours: 0,
+        facility: null,
+        holdBy: null,
+        holdByDescription: '',
+        jobStatus: null,
+        plannedFinishDate: null,
+        plannedStartupDate: null,
+        projectIdentifier: null,
+        projectProgress: null,
+        remainingHours: 0,
+        responsible: null,
+        responsibleCode: null,
+        w2ActualDate: null,
+        w3ActualDate: null,
+        w4ActualDate: null,
+        w6ActualDate: null,
+        workOrderId: woNo,
+        workOrderUrlId: null,
+        expendedHours: '0',
+    }));
+
+    return famWos.concat(notFoundWos);
 }
