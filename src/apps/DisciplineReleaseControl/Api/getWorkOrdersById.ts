a81e394d9e6b9e2692d66ac06f1bf545132df733
Joachim <joachim290@outlook.com>
2023-04-05 09:35:21 +0200

    chore: pupdate fam endpoints

--- src/apps/DisciplineReleaseControl/Api/getWorkOrdersById.ts
+++ src/apps/DisciplineReleaseControl/Api/getWorkOrdersById.ts
@@ -1,13 +1,53 @@
+import { generateExpressions, generateFamRequest } from '@equinor/fam-request-builder';
 import { httpClient } from '@equinor/lighthouse-portal-client';
 import { WorkOrder } from '../Types/workOrder';
-
+const workOrderColumns = [
+    'SourceIdentity',
+    'Facility',
+    'Project',
+    'WorkOrderNo',
+    'WorkOrderUrlId',
+    'Title',
+    'Description',
+    'PlannedStartupDate',
+    'ActualStartupDate',
+    'PlannedCompletionDate',
+    'CommissioningPackageNo',
+    'Milestone',
+    'Category',
+    'HoldBy',
+    'TypeOfWork',
+    'OnshoreOffshore',
+    'ProjectProgress',
+    'JobStatus',
+    'MaterialStatus',
+    'EstimatedManHours',
+    'ExpendedManHours',
+    'RemainingManHours',
+    'SubMilestone',
+    'IsVoided',
+    'Responsible',
+    'Location',
+    'Discipline',
+    'CreatedDate',
+    'UpdatedDate',
+];
 export async function getWorkOrdersById(id: string): Promise<WorkOrder[]> {
     if (!id || id === '') return [];
     const { FAM } = httpClient();
-
-    const res = await FAM.fetch(`v0.1/procosys/pipetest/JCA/workorders/${id}`, {
-        method: 'GET',
-    });
+    const expressions = generateExpressions(
+        'PipingRevisionMechanicalCompletionPackageNo',
+        'Equals',
+        [id]
+    );
+    const requestArgs = generateFamRequest(workOrderColumns, 'Or', expressions);
+    const res = await FAM.fetch(
+        `v1/typed/completion/customapi_pipetestworkorders/facility/JCA?view-version=v0`,
+        {
+            method: 'POST',
+            body: JSON.stringify(requestArgs),
+        }
+    );
 
     const workOrders: WorkOrder[] = await res.json();
 
