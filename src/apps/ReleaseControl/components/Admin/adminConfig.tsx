f84966404302d503e1ee0d5d1ae8c3efed62b043
Ken Mellem <110427825+ken-mellem@users.noreply.github.com>
2023-09-15 14:04:49 +0200

    enable the admin check in release control

--- src/apps/ReleaseControl/components/Admin/adminConfig.tsx
+++ src/apps/ReleaseControl/components/Admin/adminConfig.tsx
@@ -2,7 +2,7 @@ import { AdminOptions } from '@equinor/WorkSpace';
 import { Admin } from '@equinor/Admin';
 import { checkIfReleaseControlAdmin } from '@equinor/Workflow';
 
-export const adminConfig: AdminOptions = { app: 'release', component: getAdminPage(), isAdminValidator: ()=>checkIfReleaseControlAdmin('workflows') };
+export const adminConfig: AdminOptions = { app: 'release', component: getAdminPage(), isAdminValidator: () => checkIfReleaseControlAdmin('workflows') };
 
 export function getAdminPage(): JSX.Element {
     return <Admin app={'releasecontrol'} workflowOwner={'ReleaseControl'} />;
