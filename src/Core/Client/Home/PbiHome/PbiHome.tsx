08d75b7d25883558f190b1e4f48f611de0b33f99
Gustav Eikaas <guei@equinor.com>
2023-10-10 15:36:19 +0200

    fix: :bug: token not refreshing powerbi

--- src/Core/Client/Home/PbiHome/PbiHome.tsx
+++ src/Core/Client/Home/PbiHome/PbiHome.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable no-console */
 import { tokens } from '@equinor/eds-tokens';
-import { NetworkError } from '@equinor/http-client';
-import { useHttpClient } from '@equinor/lighthouse-portal-client';
-import { Embed, IReportEmbedConfiguration, models, service } from 'powerbi-client';
+import { Embed, service } from 'powerbi-client';
 import { PowerBIEmbed } from 'powerbi-client-react';
 import 'powerbi-report-authoring';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Icon from '../../../../components/Icon/Icon';
 import './style.css';
+import {
+    INTERVAL_TIME,
+    checkTokenAndUpdate,
+} from '../../../../modules/powerBI/src/Utils/updateAccessToken';
+import { Report as EmbedReport } from 'powerbi-client';
+import { usePowerBI } from '../../../../modules/powerBI/src';
 
 const Wrapper = styled.div`
     position: relative;
@@ -42,11 +46,6 @@ interface PowerBiHomeProps {
     };
 }
 
-interface PowerBIResult {
-    config: IReportEmbedConfiguration;
-    error: NetworkError | undefined;
-}
-
 type BasicFilterOperators = 'In' | 'NotIn' | 'All';
 
 export interface PowerBiFilter {
@@ -69,134 +68,13 @@ export interface Filter {
     operator: BasicFilterOperators;
 }
 
-const filterBuilder = (filter: Filter): PowerBiFilter => {
-    return {
-        $schema: 'http://powerbi.com/product/schema#basic',
-        target: {
-            table: filter.target.table,
-            column: filter.target.column,
-        },
-        filterType: 1,
-        operator: filter.operator,
-        values: filter.values,
-    };
-};
-
-interface useFusionClientReturn {
-    getConfig: () => Promise<IReportEmbedConfiguration>;
-    error: NetworkError | undefined;
-}
-export function useFusionClient(
-    resource: string,
-    filterOptions?: Filter[],
-    options?: {
-        showFilter?: boolean;
-        enablePageNavigation?: boolean;
-    }
-): useFusionClientReturn {
-    const { fusionPbi } = useHttpClient();
-    const [error, setError] = useState<NetworkError>();
-
-    const filters: PowerBiFilter[] = [];
-    filterOptions?.forEach((filterOption) => {
-        filters.push(filterBuilder(filterOption));
-    });
-
-    async function getEmbedInfo() {
-        try {
-            const embedUri = `/reports/jca-landing-page/config/embedinfo`;
-            const response = await fusionPbi.fetch(embedUri);
-
-            const data = await response.json();
-            window['embedInfo'] = data;
-            return data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function getPowerBiToken() {
-        try {
-            const tokenUri = `reports/jca-landing-page/token`;
-            const response = await fusionPbi.fetch(tokenUri);
-            return await response.json();
-        } catch (error: any) {
-            const networkError = error as NetworkError;
-            setError(networkError);
-        }
-    }
-
-    async function getConfig(): Promise<IReportEmbedConfiguration> {
-        const { embedConfig } = await getEmbedInfo();
-        const { token } = await getPowerBiToken();
-        return {
-            accessToken: token,
-            embedUrl: embedConfig.embedUrl,
-            id: embedConfig.reportId,
-            settings: {
-                panes: {
-                    filters: {
-                        expanded: false,
-                        visible: options?.showFilter ?? false,
-                    },
-                    pageNavigation: {
-                        visible: options?.enablePageNavigation ?? false,
-                    },
-                },
-            },
-            filters: filters ?? undefined,
-        };
-    }
-
-    return {
-        getConfig,
-        error,
-    };
-}
-
-export function usePowerBI(
-    resource: string,
-    filterOptions?: Filter[],
-    options?: { showFilter?: boolean; enablePageNavigation?: boolean }
-): PowerBIResult {
-    const { getConfig, error } = useFusionClient(resource, filterOptions, options);
-    const [config, setReportConfig] = useState<IReportEmbedConfiguration>({
-        type: 'report',
-        embedUrl: undefined,
-        tokenType: models.TokenType.Embed,
-        accessToken: undefined,
-        settings: undefined,
-        theme: {
-            name: 'Custom Theme',
-            background: '#FFFFFF',
-            secondaryBackground: '#C8C6C4',
-        },
-    });
-
-    useEffect(() => {
-        async function setupReportConfig() {
-            try {
-                const fusionConfig = await getConfig();
-                setReportConfig((config) => ({ ...config, ...fusionConfig }));
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        setupReportConfig();
-    }, [resource, filterOptions]);
-
-    return {
-        config,
-        error,
-    };
-}
-
 export const PowerBIHome = ({
     reportUri,
     filterOptions,
     options,
 }: PowerBiHomeProps): JSX.Element => {
     const { config, error } = usePowerBI(reportUri, filterOptions, options);
+    const [report, setReport] = useState<EmbedReport | null>(null);
 
     const eventHandlersMap = new Map([
         [
@@ -208,6 +86,17 @@ export const PowerBIHome = ({
             },
         ],
     ]);
+
+    useEffect(() => {
+        if (!report) return;
+        const intervalId = setInterval(() => {
+            checkTokenAndUpdate(reportUri, report);
+        }, INTERVAL_TIME);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [report, reportUri]);
 
     return (
         <>
@@ -227,6 +116,7 @@ export const PowerBIHome = ({
                         eventHandlers={eventHandlersMap}
                         getEmbeddedComponent={(embedObject: Embed) => {
                             window['report'] = embedObject;
+                            setReport(embedObject as unknown as EmbedReport);
                         }}
                         cssClassName="pbiEmbed"
                     />
