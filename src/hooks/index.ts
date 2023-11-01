3491c1fcb45773c49ae3e74d9686bacaa6b31140
Gustav Eikaas <guei@equinor.com>
2023-11-01 10:51:29 +0100

    chore: :fire: remove dead files

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,7 +2,6 @@ export * from './useInfiniteCachedQuery';
 export * from './QueryCache/useQueryCacheLookup';
 export * from './cancellationToken/useCancellationToken';
 export * from './edsTabs/useEdsTabs';
-export * from './useTimeoutWithCancel/useTimeooutWithCancel';
 export * from './useOutsideClick';
 export * from './useLocationKey/useLocationKey';
 export * from './useResizeObserver';
