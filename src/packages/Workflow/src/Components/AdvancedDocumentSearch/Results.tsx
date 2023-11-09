4f34d3f14a7380f49c33ea163d353ef28fb5952e
Gustav Eikaas <guei@equinor.com>
2023-11-09 10:27:24 +0100

    chore:

--- src/packages/Workflow/src/Components/AdvancedDocumentSearch/Results.tsx
+++ src/packages/Workflow/src/Components/AdvancedDocumentSearch/Results.tsx
@@ -16,7 +16,7 @@ export const Result = ({ result, handleClick, isSelected }: ResultProps): JSX.El
         <>
             <ResultItem key={result.value}>
                 <Checkbox
-                    crossOrigin={{}}
+                    crossOrigin={undefined}
                     checked={isChecked}
                     onChange={() => {
                         handleClick(result, isChecked ? 'Remove' : 'Add');
