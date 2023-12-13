58c72c072b8705a26e9fcf41cadc62ff981d3f55
MagnusGjosund <maggu@live.no>
2023-12-13 10:00:22 +0100

    add some temp comments

--- src/apps/ReleaseControl/components/Form/Inputs/RequestedBy/RequestedBy.tsx
+++ src/apps/ReleaseControl/components/Form/Inputs/RequestedBy/RequestedBy.tsx
@@ -11,6 +11,8 @@ export const RequestedByInput = (): JSX.Element => {
     const requestedBy = useAtomState((s) => s.requestedBy ?? '');
 
     return (
+        //AVSLUTTET HER
+        // add styling for the small heading over person/role search, and "(required) over to the right"
         <PCSPersonRoleSearch
             onSelect={() => updateRequestedBy(requestedBy)}
             value={requestedBy}
--- src/apps/ReleaseControl/components/Form/Inputs/RequestedBy/requestedBy.styles.ts
+++ src/apps/ReleaseControl/components/Form/Inputs/RequestedBy/requestedBy.styles.ts
@@ -1,1 +1,3 @@
 // add styling for the small heading over person/role search, and "(required) over to the right"
+
+export {};
