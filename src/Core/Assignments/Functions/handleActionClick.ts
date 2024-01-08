daf4a9a23061b5c29e4ec1ff7c2fda137b70e6f8
idrisbo <112471345+idrisbo@users.noreply.github.com>
2024-01-08 14:52:15 +0100

    feat: :sparkles: Action performed in the Action Centre now opens the respective application instead of the main page.

--- src/Core/Assignments/Functions/handleActionClick.ts
+++ src/Core/Assignments/Functions/handleActionClick.ts
@@ -1,10 +1,10 @@
 //Todo refactor file.
 const apps = new Map<string, string>();
-apps.set('ScopeChangeControl', 'change');
-apps.set('ReleaseControl', 'releaseDetails');
+apps.set('ScopeChangeControl', 'ProjectControl/change/table#change');
+apps.set('ReleaseControl', 'ConstructionAndCommissioning/release#releaseDetails');
 
 export async function handleActionClick(appName: string, identifier: string): Promise<void> {
     const actualName = apps.get(appName);
     if (!actualName) throw 'App not found';
-    window.open(`/#${actualName}/${identifier}`, '_blank');
+    window.open(`/${actualName}/${identifier}`, '_blank');
 }
