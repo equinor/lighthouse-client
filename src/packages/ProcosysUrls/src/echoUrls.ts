fb13320f35a96be578da1f427fcebadbe9d74853
idrisbo <112471345+idrisbo@users.noreply.github.com>
2024-01-19 15:16:18 +0100

    Add test URL Disable dev mode

--- src/packages/ProcosysUrls/src/echoUrls.ts
+++ src/packages/ProcosysUrls/src/echoUrls.ts
@@ -1,8 +1,12 @@
+import { isProduction } from '../../../Core/Client/Functions';
+
 interface EchoUrls {
     getEchoUrl: (tagNumber: string) => string;
 }
 
-const getBaseUrl = 'https://echo.equinor.com';
+const getBaseUrl = isProduction()
+    ? 'https://echo.equinor.com'
+    : 'https://dt-echopedia-web-test.azurewebsites.net';
 
 export const echoUrls: EchoUrls = {
     getEchoUrl: (tagNumber: string) =>
