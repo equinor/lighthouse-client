a6a6d17ec85eacb8808a497df1af9147db870ffa
Gustav Eikaas <guei@equinor.com>
2024-01-31 14:43:30 +0100

    revert ff config

--- src/fusion-framework/config.ts
+++ src/fusion-framework/config.ts
@@ -67,9 +67,9 @@ export const createConfig = (appSettings: AppConfigResult) => {
             });
         }
         Object.entries(appSettings.urls).forEach(([a, b]) => {
-            if (a == 'scopeChange' && true) {
+            if (a == 'scopeChange' && false) {
                 config.configureHttpClient(a, {
-                    baseUri: 'https://localhost:50435',
+                    baseUri: 'https://localhost:53199',
                     defaultScopes: [appSettings.scope[a]],
                 });
             } else {
