dd21677c1481430dd6a92c1b33139091bf4b8166
Gustav Eikaas <guei@equinor.com>
2024-02-07 07:48:44 +0100

    hmm

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [react()],
-
-    preview: {
-        port: 3000,
-    },
-    server: {
-        // https: true,
-        port: 3000,
-    },
     define: {
         'process.env': {},
     },
