3b962ec1fec1ffd0e1ccf132815f523513f06495
Gustav Eikaas <guei@equinor.com>
2024-02-07 14:11:36 +0100

    test

--- deployment/config.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-    local: {
-        REDIRECT_URI: 'http://localhost:3000',
-    },
-    dev: {
-        REDIRECT_URI: 'https://frontend-lighthouse-client-dev.radix.equinor.com',
-    },
-};
