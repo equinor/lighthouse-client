23918753a8fea0cbbdff7cde9280342ec22ff53c
Gustav Eikaas <guei@equinor.com>
2024-02-08 09:42:57 +0100

    make it work locally

--- src/Core/Client/Service/appConfig.ts
+++ src/Core/Client/Service/appConfig.ts
@@ -3,6 +3,12 @@ import { AppConfigResult } from '../Types/AppConfig';
 import { fetchClientConfig } from './envConfig';
 
 export async function fetchConfig(): Promise<AppConfigResult> {
+    //When running locally
+    if (import.meta.env.DEV === true) {
+        window.AUTH_CONFIG = import.meta.env.VITE_AUTH_CONFIG;
+        window.JC_CONFIG = import.meta.env.VITE_JC_CONFIG;
+    }
+
     const config = await fetchClientConfig();
 
     const isProduction = config.CLIENT_ENV === 'prod';
@@ -11,7 +17,10 @@ export async function fetchConfig(): Promise<AppConfigResult> {
     }
 
     setEnv(isProduction, config.CLIENT_ENV);
-
+    //When running locally
+    // if (import.meta.env.DEV === true) {
+    //     return JSON.parse(import.meta.env.VITE_JC_CONFIG);
+    // }
     if (!window.JC_CONFIG) {
         throw 'No JC config set';
     }
