be065f104c752251f33e949d848748d6f9927830
Gustav Eikaas <guei@equinor.com>
2024-02-08 08:34:20 +0100

    try add service message

--- src/Core/Client/Service/envConfig.ts
+++ src/Core/Client/Service/envConfig.ts
@@ -6,6 +6,7 @@ declare global {
     interface Window {
         AUTH_CONFIG: string;
         JC_CONFIG: string;
+        SERVICE_MESSAGE: string;
     }
 }
 export async function fetchClientConfig(): Promise<EnvConfig> {
