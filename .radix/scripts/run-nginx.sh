a50b795148f437f11c15c7d99c02120e107a1f32
Gustav Eikaas <guei@equinor.com>
2024-02-08 08:24:07 +0100

    update radixconfig

--- .radix/scripts/run-nginx.sh
+++ .radix/scripts/run-nginx.sh
@@ -3,6 +3,7 @@ echo "Starting NGINX"
 envsubst '
   ${JC_CONFIG}
   ${AUTH_CONFIG}
+  ${SERVICE_MESSAGE}
   ' </usr/share/nginx/html/index.html >/usr/share/nginx/html/tmp.html
 mv /usr/share/nginx/html/tmp.html /usr/share/nginx/html/index.html
 nginx -g 'daemon off;'
