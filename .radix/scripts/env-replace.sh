83e4bf1d9d74204c58f1fc1ca2f00fe4591da374
Gustav Eikaas <guei@equinor.com>
2024-02-08 07:51:13 +0100

    cleanup

--- .radix/scripts/env-replace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-envsubst '
-  ${JC_CONFIG}
-  ${AUTH_CONFIG}
-  ' </usr/share/nginx/html/index.html >/usr/share/nginx/html/tmp.html
-mv /usr/share/nginx/html/tmp.html /usr/share/nginx/html/index.html
