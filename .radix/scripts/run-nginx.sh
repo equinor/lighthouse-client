1f2aa217cb30605b68246d120e6dbe5c9703a937
Gustav Eikaas <guei@equinor.com>
2024-02-08 09:51:19 +0100

    fix env

--- .radix/scripts/run-nginx.sh
+++ .radix/scripts/run-nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Starting NGINX"
 envsubst '
-  ${JC_CONFIG}
-  ${AUTH_CONFIG}
-  ${SERVICE_MESSAGE}
+  ${JC_CONFIG ?? ''}
+  ${AUTH_CONFIG ?? ''}
+  ${SERVICE_MESSAGE ?? ''}
   ' </usr/share/nginx/html/index.html >/usr/share/nginx/html/tmp.html
 mv /usr/share/nginx/html/tmp.html /usr/share/nginx/html/index.html
 nginx -g 'daemon off;'
