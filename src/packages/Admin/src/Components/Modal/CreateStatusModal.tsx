28893a6d4176416e2794241b3a3c3492286f2ce8
Kjell Haaland <kjell.haland@bouvet.no>
2024-02-22 08:58:28 +0100

    Updated CreateStatus and EditStatus forms

--- src/packages/Admin/src/Components/Modal/CreateStatusModal.tsx
+++ src/packages/Admin/src/Components/Modal/CreateStatusModal.tsx
@@ -1,85 +1,72 @@
-import { Button, TextField } from '@equinor/eds-core-react-old';
-import { KeyboardEventHandler, useState } from 'react';
+import { Button } from '@equinor/eds-core-react';
+import { Form, FormikValues } from 'formik';
+import * as Yup from 'yup';
+
 import { useAdminContext } from '../../Hooks/useAdminContext';
 import { useAdminMutation } from '../../Hooks/useAdminMutation';
 import { useAdminMutations } from '../../Hooks/useAdminMutations';
 import { adminMutationKeys } from '../../Queries/adminMutationKeys';
 import { ModalButtonContainer, ModalInputContainer } from './modalStyles';
 
+import { FormContainer, TextField } from '../../../../EdsForm';
+
 type CreateWorkflowStatusModalProps = {
-    setIsCreating: (isCreating: boolean) => void;
+    readonly setIsCreating: (isCreating: boolean) => void;
 };
+
+const validationSchema = Yup.object().shape({
+    name: Yup.string()
+        .max(255, 'The name must be less than 255 characters!')
+        .required('(Required)'),
+});
 
 export const CreateStatusModal = ({
     setIsCreating,
 }: CreateWorkflowStatusModalProps): JSX.Element => {
     const workflowOwner = useAdminContext((s) => s.workflowOwner);
-    const [name, setName] = useState<string>('');
 
     const { createWorkflowStatusMutation } = useAdminMutations();
-
     const { postKey } = adminMutationKeys('');
-
     const { mutate } = useAdminMutation('', postKey, createWorkflowStatusMutation);
 
-    async function createStatus() {
-        mutate({
-            name: name,
-            workflowOwner: workflowOwner,
-        });
+    const onSubmit = async (values: FormikValues) => {
+        mutate({ name: values.name, workflowOwner: workflowOwner });
         setIsCreating(false);
-    }
-
-    const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value);
     };
 
-    const handleOnKeyPress: KeyboardEventHandler<HTMLDivElement> = (event) => {
-        if (event.key === 'Escape') {
-            event.preventDefault();
-            setIsCreating(false);
-        }
-        //Allow shift+enter linebreak
-        if (event.key === 'Enter' && !event.shiftKey && name !== '') {
-            event.preventDefault();
-            createStatus();
-        }
+    const onCancel = () => {
+        setIsCreating(false);
     };
 
     return (
-        <div onKeyDown={handleOnKeyPress} tabIndex={0}>
-            <ModalInputContainer>
-                <TextField
-                    variant="default"
-                    id="name"
-                    label="Name"
-                    value={name}
-                    onChange={onNameChange}
-                    multiline
-                    autoFocus={true}
-                    placeholder={'Write a name for the status'}
-                />
-            </ModalInputContainer>
-
-            <ModalButtonContainer>
-                <Button
-                    variant="contained"
-                    onClick={async () => {
-                        createStatus();
-                    }}
-                    disabled={name === ''}
-                >
-                    Create
-                </Button>
-                <Button
-                    variant="outlined"
-                    onClick={() => {
-                        setIsCreating(false);
-                    }}
-                >
-                    Cancel
-                </Button>
-            </ModalButtonContainer>
-        </div>
+        <FormContainer
+            initialValues={{}}
+            validationSchema={validationSchema}
+            validateOnMount={true}
+            onSubmit={onSubmit}
+        >
+            {({ isValid }) => (
+                <Form>
+                    <ModalInputContainer>
+                        <TextField
+                            id="name"
+                            name="name"
+                            label="Name"
+                            multiline
+                            autoFocus={true}
+                            placeholder="Write a name for the status"
+                        />
+                    </ModalInputContainer>
+                    <ModalButtonContainer>
+                        <Button type="submit" variant="contained" disabled={!isValid}>
+                            Save
+                        </Button>
+                        <Button variant="outlined" onClick={onCancel}>
+                            Cancel
+                        </Button>
+                    </ModalButtonContainer>
+                </Form>
+            )}
+        </FormContainer>
     );
 };
