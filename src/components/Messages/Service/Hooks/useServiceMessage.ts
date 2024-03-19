bc3661a0a82f2f2d25d2d7b933ab6448b4238bca
Gustav Eikaas <guei@equinor.com>
2024-03-19 16:31:28 +0100

    refactor: service message

--- src/components/Messages/Service/Hooks/useServiceMessage.ts
+++ src/components/Messages/Service/Hooks/useServiceMessage.ts
@@ -6,61 +6,59 @@ import { ServiceMessage } from '../Types/serviceMessage';
 const SM_KEY = 'serviceMessageId';
 
 interface Return {
-    message?: ServiceMessage;
-    isActive: boolean;
-    handleClose(message?: ServiceMessage): void;
+  message?: ServiceMessage;
+  isActive: boolean;
+  handleClose(message?: ServiceMessage): void;
 }
 
 export function useServiceMessage(): Return {
-    const appConfig = useHttpClient('appConfig');
+  const appConfig = useHttpClient('appConfig');
 
-    const [isActive, setIsActive] = useState<boolean>(false);
-    const [message, setMessage] = useState<ServiceMessage | undefined>();
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [message, setMessage] = useState<ServiceMessage | undefined>();
 
-    function handleClose(message?: ServiceMessage) {
-        setIsActive(false);
-        if (message) {
-            storage.setItem(SM_KEY, message.id);
-        }
+  function handleClose(message?: ServiceMessage) {
+    setIsActive(false);
+    if (message) {
+      storage.setItem(SM_KEY, message.id);
     }
+  }
 
-    function localMessage(message?: ServiceMessage): ServiceMessage | undefined {
-        const localMessageId = storage.getItem<string>(SM_KEY);
-        if (localMessageId && message?.id === localMessageId) {
-            setIsActive(false);
-            return;
-        }
-        return message;
+  function localMessage(message?: ServiceMessage): ServiceMessage | undefined {
+    const localMessageId = storage.getItem<string>(SM_KEY);
+    if (localMessageId && message?.id === localMessageId) {
+      setIsActive(false);
+      return;
     }
+    return message;
+  }
 
-    useEffect(() => {
-        (async () => {
-            const response = window.SERVICE_MESSAGE;
-            if (!response || response.length == 0) return;
-            try {
-                const message = JSON.parse(response);
-                const data: ServiceMessage = {
-                    message: message.message,
-                    type: message.type,
-                    link: undefined,
-                    id: btoa(message.message),
-                    fromDate: '2019-02-09',
-                    toDate: '2030-02-09',
-                };
-                const maybeMessage = localMessage(data);
-                if (maybeMessage && maybeMessage.id) {
-                    setMessage(maybeMessage);
-                    setIsActive(true);
-                }
-            } catch (e) {
-                console.error('Failed to load service message');
-            }
-        })();
-    }, [appConfig]);
+  useEffect(() => {
+    const stringifiedServiceMessage = window.SERVICE_MESSAGE;
+    if (!stringifiedServiceMessage || stringifiedServiceMessage.length == 0) return;
+    try {
+      const message = JSON.parse(stringifiedServiceMessage);
+      const data: ServiceMessage = {
+        message: message.message,
+        type: message.type,
+        link: undefined,
+        id: btoa(message.message),
+        fromDate: '2019-02-09',
+        toDate: '2030-02-09',
+      };
+      const maybeMessage = localMessage(data);
+      if (maybeMessage && maybeMessage.id) {
+        setMessage(maybeMessage);
+        setIsActive(true);
+      }
+    } catch (e) {
+      console.error('Failed to load service message');
+    }
+  }, [appConfig]);
 
-    return {
-        message,
-        isActive,
-        handleClose,
-    };
+  return {
+    message,
+    isActive,
+    handleClose,
+  };
 }
