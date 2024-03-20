c997d12e0e15793548d83bbfbae3e01d183d2648
Gustav Eikaas <guei@equinor.com>
2024-03-20 09:17:08 +0100

    revert

--- .radix/scripts/run-nginx.sh
+++ .radix/scripts/run-nginx.sh
@@ -2,8 +2,8 @@
 echo "Starting NGINX"
 envsubst '
   ${JC_CONFIG}
+  ${AUTH_CONFIG}
   ${SERVICE_MESSAGE}
   ' </usr/share/nginx/html/index.html >/usr/share/nginx/html/tmp.html
 mv /usr/share/nginx/html/tmp.html /usr/share/nginx/html/index.html
-export VITE_AUTH_CONFIG = ${VITE_AUTH_CONFIG}
 nginx -g 'daemon off;'
