82bf06ebc60bdc809518d8af2264b3f552aa378e
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2024-05-21 10:24:53 +0200

    fix: release control temporariliy fix punch search by reverting to v1 search (#1820)

--- src/packages/Workflow/src/Api/FAM/searchPunchListItems.ts
+++ src/packages/Workflow/src/Api/FAM/searchPunchListItems.ts
@@ -1,11 +1,17 @@
 import { httpClient } from '@equinor/lighthouse-portal-client';
+import { generateExpressions, generateFamRequest } from '../../../../FamRequestBuilder/src';
 import { PunchListItem } from '../../Types/FAMTypes';
 import { throwOnError } from '../throwOnError';
 
 const columnNames: string[] = ['PunchItemNo', 'Description'];
 const facility = 'JCA';
 const view = 'Completion.CompletionPunchItem_v1';
-export async function searchPunchListItems(
+
+
+/**
+* Keeping double implementation for a while since FAM is experiencing issues from time to time. V2 is the preffered view to avoid stale data. 
+*/
+export async function searchPunchListItemsV2(
   id: string,
   signal?: AbortSignal
 ): Promise<PunchListItem[]> {
@@ -37,4 +43,37 @@ ORDER BY facility asc`,
   }
 
   return punchListItems.data;
+}
+
+export async function searchPunchListItemsV1(
+    id: string,
+    signal?: AbortSignal
+): Promise<PunchListItem[]> {
+    const { FAM } = httpClient();
+
+    const columnNames: string[] = ['PunchItemNo', 'Description'];
+
+    const expressions = generateExpressions('PunchItemNo', 'Like', [id]);
+
+    const requestArgs = generateFamRequest(columnNames, 'Or', expressions, { take: 50, skip: 0 });
+
+    const res = await FAM.fetch(
+        'v1/typed/completion/completionPunchItem/facility/JCA?view-version=v1',
+        {
+            method: 'POST',
+            headers: { ['content-type']: 'application/json' },
+            body: JSON.stringify(requestArgs),
+            signal,
+        }
+    );
+
+    await throwOnError(res, 'Failed to get punch list items');
+
+    const punchListItems: PunchListItem[] = await res.json();
+
+    if (!Array.isArray(punchListItems)) {
+        throw 'Invalid response';
+    }
+
+    return punchListItems;
 }
